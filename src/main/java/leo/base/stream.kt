d68a2b86936a2bd04dd9d67dc76c5b7cfaa3d330
Michał Pociecha-Łoś <micapolos@gmail.com>
2018-11-14 21:41:30 +0100

    stack map

--- src/main/java/leo/base/stream.kt
+++ src/main/java/leo/base/stream.kt
@@ -16,6 +16,9 @@ val <V> Stream<V>.next
 fun <V> Stream<V>.plus(value: V) =
 	Stream(value) { this }
 
+fun <V> stream(first: V, vararg next: V) =
+	stack(first, *next).stream
+
 fun <V, R> Stream<V>.foldFirst(fn: (V) -> R): Stream.FoldedFirst<V, R> =
 	Stream.FoldedFirst(fn(first), nextFn())
 
@@ -26,6 +29,9 @@ tailrec fun <V, R> R.fold(streamOrNull: Stream<V>?, foldNext: R.(V) -> R): R =
 	if (streamOrNull == null) this
 	else foldNext(streamOrNull.first).fold(streamOrNull.nextFn(), foldNext)
 
+fun <V, R> Stream<V>.map(fn: (V) -> R): Stream<R> =
+	Stream(fn(first)) { next?.map(fn) }
+
 val <V> Stream<V>.reversedStack
 	get() =
 		foldFirst { it.stack }.foldNext { push(it) }
@@ -33,6 +39,10 @@ val <V> Stream<V>.reversedStack
 val <V> Stream<V>.reverse
 	get() =
 		reversedStack.stream
+
+val <V> Stream<V>.stack
+	get() =
+		reversedStack.reverse
 
 val <V> Stack<V>.stream: Stream<V>
 	get() =
--- src/test/java/leo/base/streamTest.kt
+++ src/test/java/leo/base/streamTest.kt
@@ -5,7 +5,7 @@ import kotlin.test.Test
 class StreamTest {
 	@Test
 	fun stackStream() {
-		val stream1 = stack(1, 2, 3).stream
+		val stream1 = stream(1, 2, 3)
 		stream1.first.assertEqualTo(3)
 		val stream2 = stream1.next!!
 		stream2.first.assertEqualTo(2)
@@ -16,8 +16,7 @@ class StreamTest {
 
 	@Test
 	fun fold() {
-		stack('a', 'b', 'c')
-			.stream
+		stream('a', 'b', 'c')
 			.foldFirst(Char::toString)
 			.foldNext(String::plus)
 			.assertEqualTo("cba")
@@ -25,9 +24,18 @@ class StreamTest {
 
 	@Test
 	fun reversedStack() {
-		stack(1, 2, 3)
-			.stream
+		stream(1, 2, 3)
 			.reversedStack
 			.assertEqualTo(stack(3, 2, 1))
 	}
+
+	@Test
+	fun map() {
+		stream(1, 2, 3)
+			.map(Int::toString)
+			.assertContains("1", "2", "3")
+	}
 }
+
+fun <V> Stream<V>.assertContains(first: V, vararg next: V) =
+	stack.assertEqualTo(stack(first, *next))
