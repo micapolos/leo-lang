8cdab1f05ece38b01880356643e7bd1a6c3dcb25
Michał Pociecha-Łoś <micapolos@gmail.com>
2018-11-21 21:57:37 +0100

    parse

---
+++ src/main/java/leo/base/parse.kt
@@ -0,0 +1,11 @@
+package leo.base
+
+data class Parse<V, P>(
+	val streamOrNull: Stream<V>?,
+	val parsed: P)
+
+infix fun <V, P> Stream<V>?.parsed(value: P) =
+	Parse(this, value)
+
+fun <V, P, R> Parse<V, P>.map(fn: (P) -> R): Parse<V, R> =
+	Parse(streamOrNull, fn(parsed))
