533a04e167e15bf645c1388b4c68d7a58957a9b3
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-03-17 19:26:00 +0100

    vm test

--- src/test/java/leo32/vmTest.kt
+++ src/test/java/leo32/vmTest.kt
@@ -7,17 +7,34 @@ class VmTest {
 	@Test
 	fun vm() {
 		val vm = newVm
-		newVmCapacity.assertEqualTo(65536)
-
 		vm.top.assertEqualTo(0)
-		vm.mem.size.assertEqualTo(65536)
 
-		vm.alloc(65535) {}
-		vm.top.assertEqualTo(65535)
-		vm.mem.size.assertEqualTo(65536)
+		vm.alloc(3) { top ->
+			top.assertEqualTo(3)
+			vm.set(top, 0, 1)
+			vm.set(top, 1, 2)
+			vm.set(top, 2, 3)
+		}
 
-		vm.alloc(1) {}
-		vm.top.assertEqualTo(65536)
-		vm.mem.size.assertEqualTo(131072)
+		vm.top.assertEqualTo(3)
+		vm.get(vm.top).assertEqualTo(1)
+		vm.get(vm.top, 1).assertEqualTo(2)
+		vm.get(vm.top, 2).assertEqualTo(3)
+
+		vm.alloc(1) { top ->
+			top.assertEqualTo(4)
+			vm.set(top, 0, 4)
+		}
+
+		vm.top.assertEqualTo(4)
+		vm.get(vm.top).assertEqualTo(4)
+		vm.get(vm.top, 1).assertEqualTo(1)
+		vm.get(vm.top, 2).assertEqualTo(2)
+		vm.get(vm.top, 3).assertEqualTo(3)
+
+		vm.alloc(1 shl 24) { top ->
+			top.assertEqualTo(4 + (1 shl 24))
+		}
+		vm.top.assertEqualTo(4 + (1 shl 24))
 	}
 }
