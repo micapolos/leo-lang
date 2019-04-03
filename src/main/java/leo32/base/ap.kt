7a0b14673398be89ab119c1598efaf9c3ab5277d
micapolos <micapolos@gmail.com>
2019-04-03 16:58:27 +0200

    - ap - term

--- src/main/java/leo32/base/ap.kt
+++
@@ -1,8 +0,0 @@
-package leo32.base
-
-data class Ap<out T, out V>(
-	val target: T,
-	val value: V)
-
-fun <T, V> T.ap(value: V) =
-	Ap(this, value)
--- src/test/java/leo32/base/termTest.kt
+++ src/test/java/leo32/base/termTest.kt
@@ -1,12 +1,28 @@
 package leo32.base
 
+import leo.base.assertEqualTo
+import leo.base.string
 import kotlin.test.Test
 
 class TermTest {
 	@Test
-	fun test() {
-		"zero".term.plus("bit".term)
-			.plus("and".term.plus("zero".term.plus("bit".term)))
-			.plus("gives".term.plus("zero".term.plus("bit".term)))
+	fun string() {
+		term("zero", "bit")
+			.term("and" fieldTo term("zero", "bit"))
+			.term("or" fieldTo term("zero", "bit")
+				.term("and" fieldTo term("one", "bit")))
+			.term("gives" fieldTo term("zero", "bit"))
+			.string.assertEqualTo("zero.bit.and(zero.bit).or(zero.bit.and(one.bit)).gives(zero.bit)")
+	}
+
+	@Test
+	fun structString() {
+		term("zero", "i32", "y")
+			.term("x" fieldTo term("zero", "i32"))
+			.term("center".field)
+			.term("radius" fieldTo term("zero"))
+			.term("circle".field)
+			.string
+			.assertEqualTo("circle(radius: zero, center(x: zero, y: zero))")
 	}
 }
