3d69dddbb0879384698e156e58a11cac45379e61
micapolos <micapolos@gmail.com>
2019-04-06 12:17:47 +0200

    test arch

--- src/test/java/leo/arch/wordTest.kt
+++ src/test/java/leo/arch/wordTest.kt
@@ -6,16 +6,28 @@ import org.junit.Test
 
 class WordTest {
 	@Test
-	fun incDec() {
-		archTest { archTestIncDec }
+	fun all() {
+		archTest {
+			this
+				.run { zero.assertFails { dec } }
+				.run { zero.inc.dec.assertEqualTo(zero) }
+				.run { zero.inc.inc.dec.dec.assertEqualTo(zero) }
+
+				.run { max.assertFails { inc } }
+				.run { max.dec.inc.assertEqualTo(max) }
+				.run { max.dec.dec.inc.inc.assertEqualTo(max) }
+
+				.run { zero.plus(zero).assertEqualTo(zero) }
+				.run { zero.plus(zero.inc).assertEqualTo(zero.inc) }
+				.run { zero.plus(zero.inc.inc).assertEqualTo(zero.inc.inc) }
+				.run { max.plus(zero).assertEqualTo(max) }
+				// TODO .run { max.assertFails { plus(zero.inc) } }
+
+				.run { max.minus(zero).assertEqualTo(max) }
+				.run { max.minus(zero.inc).assertEqualTo(max.dec) }
+				.run { max.minus(zero.inc.inc).assertEqualTo(max.dec.dec) }
+				.run { zero.minus(zero).assertEqualTo(zero) }
+			  // TODO .run { zero.assertFails { minus(zero.inc) } }
+		}
 	}
 }
-
-val <A: Arch> A.archTestIncDec get() =
-	this
-		.run { zero.assertFails { dec } }
-		.run { zero.inc.dec.assertEqualTo(zero) }
-		.run { zero.inc.inc.dec.dec.assertEqualTo(zero) }
-		.run { max.assertFails { inc } }
-		.run { max.dec.inc.assertEqualTo(max) }
-		.run { max.dec.dec.inc.inc.assertEqualTo(max) }
