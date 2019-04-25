2aef37433a6faa66604ad315cfa9110ecd2062bf
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-04-25 23:46:56 +0200

    remove more stuff

--- src/main/java/leo32/list32.kt
+++
@@ -1,5 +0,0 @@
-package leo32
-
-import leo32.base.I32
-
-typealias List32 = List<I32>
--- src/main/java/leo32/script.kt
+++ src/main/java/leo32/script.kt
@@ -1,13 +1,8 @@
 package leo32
 
-import leo.base.appendableString
-
 data class Script(
 	val string: String)
 
 val String.script
 	get() =
 		Script(this)
-
-fun script(fn: ScriptWriter.() -> ScriptWriter) =
-	appendableString { it.scriptWriter.fn() }.script
