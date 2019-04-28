43c67a7d6172295caceeb9d423a4e9084543ca62
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-04-28 23:06:18 +0200

    bit math test

--- src/test/java/leo32/treo/bitNegateTest.kt
+++ src/test/java/leo32/treo/bitNegateTest.kt
@@ -1,7 +1,6 @@
 package leo32.treo
 
 import leo.base.assertEqualTo
-import leo.binary.*
 import kotlin.test.Test
 
 val lhsBitVar = newVar()
@@ -119,25 +118,26 @@ val mathTreo =
 class BitNegateTest {
 	@Test
 	fun testMathTreo() {
-		mathTreo.invoke("000").assertEqualTo(bit0.inverse.digitString)
-		mathTreo.invoke("100").assertEqualTo(bit1.inverse.digitString)
+		mathTreo.invoke("000").assertEqualTo("1") // !0 => 1
+		mathTreo.invoke("100").assertEqualTo("0") // !1 => 0
 
-		mathTreo.invoke("0010").assertEqualTo(bit0.and(bit0).digitString)
-		mathTreo.invoke("0011").assertEqualTo(bit0.and(bit1).digitString)
-		mathTreo.invoke("1010").assertEqualTo(bit1.and(bit0).digitString)
-		mathTreo.invoke("1011").assertEqualTo(bit1.and(bit1).digitString)
+		mathTreo.invoke("0010").assertEqualTo("0") // 0&0 => 0
+		mathTreo.invoke("0011").assertEqualTo("0") // 0&1 => 0
+		mathTreo.invoke("1010").assertEqualTo("0") // 1&0 => 0
+		mathTreo.invoke("1011").assertEqualTo("1") // 1&1 => 1
 
-		mathTreo.invoke("0100").assertEqualTo(bit0.xor(bit0).digitString)
-		mathTreo.invoke("0101").assertEqualTo(bit0.xor(bit1).digitString)
-		mathTreo.invoke("1100").assertEqualTo(bit1.xor(bit0).digitString)
-		mathTreo.invoke("1101").assertEqualTo(bit1.xor(bit1).digitString)
+		mathTreo.invoke("0100").assertEqualTo("0") // 0^0 => 0
+		mathTreo.invoke("0101").assertEqualTo("1") // 0^1 => 1
+		mathTreo.invoke("1100").assertEqualTo("1") // 1^0 => 1
+		mathTreo.invoke("1101").assertEqualTo("0") // 1^1 => 0
 
-		mathTreo.invoke("0110").assertEqualTo(bit0.or(bit0).digitString)
-		mathTreo.invoke("0111").assertEqualTo(bit0.or(bit1).digitString)
-		mathTreo.invoke("1110").assertEqualTo(bit1.or(bit0).digitString)
-		mathTreo.invoke("1111").assertEqualTo(bit1.or(bit1).digitString)
+		mathTreo.invoke("0110").assertEqualTo("0") // 0|0 => 0
+		mathTreo.invoke("0111").assertEqualTo("1") // 0|1 => 1
+		mathTreo.invoke("1110").assertEqualTo("1") // 1|0 => 1
+		mathTreo.invoke("1111").assertEqualTo("1") // 1|1 => 1
 
-		mathTreo.invoke("00000").assertEqualTo(bit0.inverse.inverse.digitString)
-		mathTreo.invoke("0000000").assertEqualTo(bit0.inverse.inverse.inverse.digitString)
+		mathTreo.invoke("00000").assertEqualTo("0") // !!0 => 0
+		mathTreo.invoke("0000000").assertEqualTo("1") // !!!0 => 1
+		mathTreo.invoke("000010").assertEqualTo("0") // !0&0 => 0
 	}
 }
