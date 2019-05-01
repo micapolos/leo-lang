c501a975a381da6355d4a586cfb2907c62a0feaf
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-05-01 12:51:26 +0200

    encode decode

--- src/test/java/leo32/treo/bitNegateTest.kt
+++ src/test/java/leo32/treo/bitNegateTest.kt
@@ -68,13 +68,28 @@ class BitNegateTest {
 
 	private val andLhsVar = newVar()
 	private val andRhsVar = newVar()
-	private val andTreo = treo(andLhsVar, treo(
-		call(negFn, param(treo(andLhsVar, treo(leaf)))),
-		treo(andLhsVar, treo(andRhsVar, treo(
-			call(negFn, param(treo(andRhsVar, treo(leaf)))),
-			treo(andRhsVar, treo(
-				call(nandFn, param(treo(andLhsVar, treo(andRhsVar, treo(leaf))))),
-				treo(leaf))))))))
+	private val andUsingNandTreo = treo(andLhsVar, treo(andRhsVar, treo(
+		call(nandFn, param(treo(andLhsVar, treo(andRhsVar, treo(leaf))))),
+		treo(andLhsVar, treo(
+			call(negFn, param(treo(andLhsVar, treo(leaf)))),
+			selfTreo)))))
+	private val andUsingNandFn = fn(andUsingNandTreo)
+
+	@Test
+	fun andUsingNand() {
+		andUsingNandFn.invoke(zeroZero).assertEqualTo(zero)
+		andUsingNandFn.invoke(zeroOne).assertEqualTo(zero)
+		andUsingNandFn.invoke(oneZero).assertEqualTo(zero)
+		andUsingNandFn.invoke(oneOne).assertEqualTo(one)
+	}
+
+	private val andTreo = treo(
+		at0(treo(
+			at0(treo(at0(treo(leaf)))),
+			at1(treo(at0(treo(leaf)))))),
+		at1(treo(
+			at0(treo(at0(treo(leaf)))),
+			at1(treo(at1(treo(leaf)))))))
 	private val andFn = fn(andTreo)
 
 	@Test
@@ -85,10 +100,184 @@ class BitNegateTest {
 		andFn.invoke(oneOne).assertEqualTo(one)
 	}
 
+	private val orTreo = treo(
+		at0(treo(
+			at0(treo(at0(treo(leaf)))),
+			at1(treo(at1(treo(leaf)))))),
+		at1(treo(
+			at0(treo(at1(treo(leaf)))),
+			at1(treo(at1(treo(leaf)))))))
+	private val orFn = fn(orTreo)
+
+	@Test
+	fun or() {
+		orFn.invoke(zeroZero).assertEqualTo(zero)
+		orFn.invoke(zeroOne).assertEqualTo(one)
+		orFn.invoke(oneZero).assertEqualTo(one)
+		orFn.invoke(oneOne).assertEqualTo(one)
+	}
+
+	private val xorTreo = treo(
+		at0(treo(
+			at0(treo(at0(treo(leaf)))),
+			at1(treo(at1(treo(leaf)))))),
+		at1(treo(
+			at0(treo(at1(treo(leaf)))),
+			at1(treo(at0(treo(leaf)))))))
+	private val xorFn = fn(xorTreo)
+
+	@Test
+	fun xor() {
+		xorFn.invoke(zeroZero).assertEqualTo(zero)
+		xorFn.invoke(zeroOne).assertEqualTo(one)
+		xorFn.invoke(oneZero).assertEqualTo(one)
+		xorFn.invoke(oneOne).assertEqualTo(zero)
+	}
+
+	private val zeroChar = treo(
+		at0(treo(
+			at0(treo(
+				at1(treo(
+					at1(treo(
+						at0(treo(
+							at0(treo(
+								at0(treo(
+									at0(treo(leaf)))))))))))))))))
+
+	private val oneChar = treo(
+		at0(treo(
+			at0(treo(
+				at1(treo(
+					at1(treo(
+						at0(treo(
+							at0(treo(
+								at0(treo(
+									at1(treo(leaf)))))))))))))))))
+
+	private val encodeBitTreo = treo(
+		at0(treo(
+			at0(treo(
+				at1(treo(
+					at1(treo(
+						at0(treo(
+							at0(treo(
+								at0(treo(
+									at0(zero),
+									at1(one))))))))))))))))
+	private val encodeBitFn = fn(encodeBitTreo)
+
+	@Test
+	fun encodeBit() {
+		encodeBitFn.invoke(zeroChar).assertEqualTo(zero)
+		encodeBitFn.invoke(oneChar).assertEqualTo(one)
+	}
+
+	private val decodeBitTreo = treo(at0(zeroChar), at1(oneChar))
+	private val decodeBitFn = fn(decodeBitTreo)
+
+	@Test
+	fun decodeBit() {
+		decodeBitFn.invoke(zero).assertEqualTo(zeroChar)
+		decodeBitFn.invoke(one).assertEqualTo(oneChar)
+	}
+
+	val notChar = treo(
+		at0(treo(
+			at0(treo(
+				at1(treo(
+					at0(treo(
+						at0(treo(
+							at0(treo(
+								at0(treo(
+									at1(treo(leaf)))))))))))))))))
+
+	val andChar = treo(
+		at0(treo(
+			at0(treo(
+				at1(treo(
+					at0(treo(
+						at0(treo(
+							at1(treo(
+								at1(treo(
+									at0(treo(leaf)))))))))))))))))
+
+	val orChar = treo(
+		at0(treo(
+			at1(treo(
+				at1(treo(
+					at1(treo(
+						at1(treo(
+							at1(treo(
+								at0(treo(
+									at0(treo(leaf)))))))))))))))))
+
+	val xorChar = treo(
+		at0(treo(
+			at1(treo(
+				at0(treo(
+					at1(treo(
+						at1(treo(
+							at1(treo(
+								at1(treo(
+									at0(treo(leaf)))))))))))))))))
+
 	private val notOp = treo(at0(treo(at0(treo(leaf)))))
 	private val andOp = treo(at0(treo(at1(treo(leaf)))))
 	private val orOp = treo(at1(treo(at0(treo(leaf)))))
 	private val xorOp = treo(at1(treo(at1(treo(leaf)))))
+
+	private val encodeOpTreo =
+		treo(
+			at0(treo(
+				at0(treo(
+					at1(treo(
+						at0(treo(
+							at0(treo(
+								at0(treo(
+									at0(treo(
+										at1(notOp))))),
+								at1(treo(
+									at1(treo(
+										at0(andOp))))))))))))),
+				at1(treo(
+					at0(treo(
+						at1(treo(
+							at1(treo(
+								at1(treo(
+									at1(treo(
+										at0(xorOp))))))))))),
+					at1(treo(
+						at1(treo(
+							at1(treo(
+								at1(treo(
+									at0(treo(
+										at0(orOp))))))))))))))))
+	private val encodeOpFn = fn(encodeOpTreo)
+
+	@Test
+	fun encodeOp() {
+		encodeOpFn.invoke(notChar).assertEqualTo(notOp)
+		encodeOpFn.invoke(andChar).assertEqualTo(andOp)
+		encodeOpFn.invoke(orChar).assertEqualTo(orOp)
+		encodeOpFn.invoke(xorChar).assertEqualTo(xorOp)
+	}
+
+	private val decodeOpTreo = treo(
+		at0(treo(
+			at0(notChar),
+			at1(andChar))),
+		at1(treo(
+			at0(orChar),
+			at1(xorChar))))
+	private val decodeOpFn = fn(decodeOpTreo)
+
+	@Test
+	fun decodeOp() {
+		decodeOpFn.invoke(notOp).assertEqualTo(notChar)
+		decodeOpFn.invoke(andOp).assertEqualTo(andChar)
+		decodeOpFn.invoke(orOp).assertEqualTo(orChar)
+		decodeOpFn.invoke(xorOp).assertEqualTo(xorChar)
+	}
 
 	private val bitMath =
 		treo(
@@ -170,52 +359,5 @@ class BitNegateTest {
 		bitMath.invoke("00000").assertEqualTo("0") // !!0 => 0
 		bitMath.invoke("0000000").assertEqualTo("1") // !!!0 => 1
 		bitMath.invoke("000010").assertEqualTo("0") // !0&0 => 0
-	}
-
-	val lhsBitVar = newVar()
-	val rhsBitVar = newVar()
-
-	val encodeBitCharFn =
-		fn(treo(
-			at0(treo(
-				at0(treo(
-					at1(treo(
-						at1(treo(
-							at0(treo(
-								at0(treo(
-									at0(treo(
-										at0(zero),
-										at1(one)))))))))))))))))
-
-	val encodeOpCharFn =
-		fn(treo(
-			at0(treo(
-				at0(treo(
-					at1(treo(
-						at0(treo(
-							at0(treo(
-								at0(treo(
-									at0(treo(
-										at1(notOp))))),
-								at1(treo(
-									at1(treo(
-										at0(andOp))))))))))))))),
-			at1(treo(
-				at0(treo(
-					at1(treo(
-						at1(treo(
-							at1(treo(
-								at1(treo(
-									at0(xorOp))))))))))),
-				at1(treo(
-					at1(treo(
-						at1(treo(
-							at1(treo(
-								at0(treo(
-									at0(orOp)))))))))))))))
-
-	@Test
-	fun charMath() {
-
 	}
 }
