7e874ae297e28a52ad7ed5c591687c2edf73b64a
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-06-10 00:19:26 +0200

    leo4

--- src/main/java/leo5/core/at.kt
+++ src/main/java/leo5/core/at.kt
@@ -3,6 +3,5 @@ package leo5.core
 import leo.binary.Bit
 
 data class At(val function: Function, val bit: Bit)
-
 fun at(function: Function, bit: Bit) = At(function, bit)
 fun At.invoke(parameter: Value) = function.invoke(parameter).at(bit)
