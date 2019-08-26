c1cc5268ba59bb250c712cb142b21e6e4977b9e8
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-08-26 19:49:39 +0200

    leo13

--- src/main/java/leo13/script/evaluator/bindings.kt
+++ src/main/java/leo13/script/evaluator/bindings.kt
@@ -3,6 +3,8 @@ package leo13.script.evaluator
 import leo13.Value
 import leo13.asScript
 import leo13.lineTo
+import leo13.script.Expr
+import leo13.value
 import leo9.Stack
 import leo9.push
 import leo9.stack
@@ -16,3 +18,6 @@ data class Bindings(val stack: Stack<Value>) {
 val Stack<Value>.bindings get() = Bindings(this)
 fun Bindings.push(value: Value) = stack.push(value).bindings
 fun bindings(vararg values: Value) = stack(*values).bindings
+
+fun Bindings.evaluate(expr: Expr): Value =
+	evaluator(this, value()).evaluate(expr)
