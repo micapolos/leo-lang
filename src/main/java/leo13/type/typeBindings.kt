266f21105ebf01b9927efa3c64f2467983f2891e
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-08-26 20:56:06 +0200

    leo13

--- src/main/java/leo13/type/typeBindings.kt
+++ src/main/java/leo13/type/typeBindings.kt
@@ -16,12 +16,8 @@ data class TypeBindings(val stack: Stack<Type>) : Scriptable() {
 val Stack<Type>.typeBindings get() = TypeBindings(this)
 fun TypeBindings.push(type: Type) = stack.push(type).typeBindings
 fun typeBindings(vararg types: Type) = stack(*types).typeBindings
-fun bindings(type: Type, vararg types: Type) = nonEmptyStack(type, *types).typeBindings
 
 val TypeBindings.asScript get() = stack.map { "bind" lineTo scriptableBody }.script
 
 fun TypeBindings.typeOrNull(given: Given): Type? =
 	stack.drop(given.previousStack)?.linkOrNull?.value
-
-fun TypeBindings.typeOrError(given: Given): Type =
-	typeOrNull(given) ?: error("argument not bound")
