76d78402b1c1fdce423e1e7aa5c4ab168de487b4
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-08-31 22:54:47 +0200

    leo13

--- src/test/java/leo13/evalTest.kt
+++ src/test/java/leo13/evalTest.kt
@@ -9,5 +9,11 @@ class EvalTest {
 		"vec(x(zero())y(one()))x()".eval.assertEqualTo("x(zero())")
 		"vec(x(zero())y(one()))y()".eval.assertEqualTo("y(one())")
 		"zero()gives(one())zero()".eval.assertEqualTo("one()")
+
+		"zero()of(zero()or(one()))switch(zero()gives(foo()of(foo()or(bar())))one()gives(bar()of(foo()or(bar()))))".eval.assertEqualTo("foo()")
+		"one()of(zero()or(one()))switch(zero()gives(foo()of(foo()or(bar())))one()gives(bar()of(foo()or(bar()))))".eval.assertEqualTo("bar()")
+
+		"zero()or(one())exists()zero()switch(zero()gives(one())one()gives(zero()))".eval.assertEqualTo("one()")
+		"zero()or(one())exists()one()switch(zero()gives(one())one()gives(zero()))".eval.assertEqualTo("zero()")
 	}
 }
