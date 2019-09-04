f96608729b944b05b457737edb2d4e24ad9d9817
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-09-04 07:49:09 +0200

    leo13

---
+++ src/main/java/leo13/base/link.kt
@@ -0,0 +1,18 @@
+package leo13.base
+
+import leo.base.Seq
+import leo.base.SeqNode
+import leo.base.then
+import leo13.LeoObject
+import leo13.Scriptable
+import leo13.orNullAsScript
+import leo13.script.plus
+
+data class Link<out V : Scriptable>(val lhsOrNull: Link<V>?, val value: V) : LeoObject() {
+	override fun toString() = super.toString()
+	override val scriptableName get() = "link"
+	override val scriptableBody get() = lhsOrNull.orNullAsScript.plus(value.scriptableLine)
+}
+
+infix fun <V : Scriptable> Link<V>?.linkTo(value: V) = Link(this, value)
+val <V : Scriptable> Link<V>.seqNode: SeqNode<V> get() = value then Seq { lhsOrNull?.seqNode }
---
+++ src/main/java/leo13/base/list.kt
@@ -0,0 +1,18 @@
+package leo13.base
+
+import leo.base.Seq
+import leo.base.fold
+import leo13.LeoObject
+import leo13.Scriptable
+import leo13.orNullAsScript
+
+data class List<out V : Scriptable>(val linkOrNull: Link<V>?) : LeoObject() {
+	override fun toString() = super.toString()
+	override val scriptableName get() = "list"
+	override val scriptableBody get() = linkOrNull.orNullAsScript
+}
+
+fun <V : Scriptable> list(linkOrNull: Link<V>?) = List(linkOrNull)
+fun <V : Scriptable> List<V>.plus(value: V) = List(linkOrNull.linkTo(value))
+fun <V : Scriptable> list(vararg values: V) = list<V>(null).fold(values) { plus(it) }
+val <V : Scriptable> List<V>.seq: Seq<V> get() = Seq { linkOrNull?.seqNode }
