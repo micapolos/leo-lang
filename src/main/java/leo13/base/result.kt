999969e46ab0c8859eeda3761a5d0631cb5d310a
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-09-11 22:09:33 +0200

    leo13

---
+++ src/main/java/leo13/base/result.kt
@@ -0,0 +1,29 @@
+package leo13.base
+
+import leo13.Scriptable
+import leo13.script.Script
+import leo13.script.script
+
+sealed class Result<out V : Scriptable> : Scriptable {
+	override val scriptableName
+		get() = when (this) {
+			is OkResult ->
+				if (value.scriptableName == "error") "meta"
+				else value.scriptableName
+			is ErrorResult -> "error"
+		}
+
+	override val scriptableBody
+		get() = when (this) {
+			is OkResult ->
+				if (value.scriptableName == "error") script(value.scriptableLine)
+				else value.scriptableBody
+			is ErrorResult -> script
+		}
+}
+
+data class OkResult<V : Scriptable>(val value: V) : Result<V>()
+data class ErrorResult<V : Scriptable>(val script: Script) : Result<V>()
+
+fun <V : Scriptable> ok(value: V): Result<V> = OkResult(value)
+fun <V : Scriptable> error(script: Script): Result<V> = ErrorResult(script)
