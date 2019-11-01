ff57b755a90bcb4abc9b82a085f8f01da2ac27bf
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-11-01 21:02:37 +0100

    leo13

--- src/main/java/leo13/js2/dsl/dsl.kt
+++ src/main/java/leo13/js2/dsl/dsl.kt
@@ -6,9 +6,13 @@ data class Function(val args: Args)
 
 val window = expr(id("window"))
 val console = expr(id("console"))
+val event = expr(id("event"))
 
 val Expr.log get() = get("log")
 val Expr.onload get() = get("onload")
+val Expr.onkeypress get() = get("keypress")
 
-fun function(vararg names: String) = Function(args(*names))
-fun Function.returns(vararg exprs: Expr) = expr(Fn(args, seq(*exprs)))
+fun fn(vararg names: String) = Function(args(*names))
+
+infix fun Function.ret(expr: Expr) = expr(Fn(args, seq(expr)))
+fun Function.does(vararg exprs: Expr) = expr(Fn(args, seq(*exprs)))
--- src/test/java/leo13/js2/dsl/dslTest.kt
+++ src/test/java/leo13/js2/dsl/dslTest.kt
@@ -1,22 +1,15 @@
 package leo13.js2.dsl
 
 import leo.base.assertEqualTo
-import leo13.js2.code
-import leo13.js2.expr
-import leo13.js2.invoke
-import leo13.js2.set
+import leo13.js2.*
 import kotlin.test.Test
 
 class DslTest {
 	@Test
 	fun code() {
-		window
-			.onload
-			.set(function().returns(
-				console.log(expr(1.0)),
-				console.log(expr("jajeczko"))
-			))
-			.code
-			.assertEqualTo(null)
+		seq(
+			window.onload.set(fn() ret console.log(expr(1.0))),
+			window.onkeypress.set(fn("event") ret console.log(event))
+		).code.assertEqualTo(null)
 	}
 }
