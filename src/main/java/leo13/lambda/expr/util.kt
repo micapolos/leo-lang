86ddcdc231347d6b9fbcbb71d9f73c74666d900b
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-11-02 19:37:38 +0100

    leo13

--- src/main/java/leo13/lambda/expr/util.kt
+++ src/main/java/leo13/lambda/expr/util.kt
@@ -4,6 +4,8 @@ import leo13.lambda.Expr
 
 fun <T> first(): Expr<T> = fn2(arg(1))
 fun <T> second(): Expr<T> = fn2(arg(0))
+
 fun <T> pair(lhs: Expr<T>, rhs: Expr<T>) = fn3(arg<T>(0)(arg(2))(arg(1)))(lhs)(rhs)
-val <T> Expr<T>.first get() = invoke(first())
-val <T> Expr<T>.second get() = invoke(second())
+
+val <T> Expr<T>.first get() = this(first())
+val <T> Expr<T>.second get() = this(second())
