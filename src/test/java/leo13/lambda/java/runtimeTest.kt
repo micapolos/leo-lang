c3111d0370a96ca12a2f8787e12e07cc2ec1a0cb
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-11-02 13:21:05 +0100

    leo13

--- src/test/java/leo13/lambda/java/runtimeTest.kt
+++ src/test/java/leo13/lambda/java/runtimeTest.kt
@@ -1,11 +1,22 @@
 package leo13.lambda.java
 
 import leo.base.assertEqualTo
+import leo13.lambda.ap
+import leo13.lambda.fn
 import kotlin.test.Test
 
 class RuntimeTest {
 	@Test
-	fun eval() {
+	fun evalHelloWorld() {
 		expr("Hello, world!").eval.assertEqualTo("Hello, world!")
+	}
+
+	@Test
+	fun evalIdentity() {
+		leo13.lambda.expr(ap(
+			leo13.lambda.expr(fn(leo13.lambda.expr(arg))),
+			expr("Hello, world!")))
+			.eval
+			.assertEqualTo("Hello, world!")
 	}
 }
