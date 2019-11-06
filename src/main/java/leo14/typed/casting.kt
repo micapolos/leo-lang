927be56822d87ecbfb8262067a09aa9091d9b505
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-11-06 23:39:28 +0100

    leo13

--- src/main/java/leo14/typed/casting.kt
+++ src/main/java/leo14/typed/casting.kt
@@ -1,33 +1,43 @@
 package leo14.typed
 
-import leo13.linkOrNull
+import leo.base.ifOrNull
+import leo.base.notNullIf
 import leo14.lambda.Term
-import leo14.lambda.pair
 
-fun <T> Term<T>.cast(fromType: Type, toType: Type): Typed<T>? =
-	fromType.lineStack.linkOrNull.let { fromLinkOrNull ->
-		toType.lineStack.linkOrNull.let { toLinkOrNull ->
-			if (fromLinkOrNull == null) {
-				if (toLinkOrNull == null) {
-					this of emptyType
-				} else {
-					null
-				}
-			} else {
-				if (toLinkOrNull == null) {
-					null
-				} else {
-					pair().run {
-						first.cast(fromLinkOrNull.stack.type, toLinkOrNull.stack.type)?.let { castLhs ->
-							second.cast(fromLinkOrNull.value, toLinkOrNull.value)?.let { castRhs ->
-								TODO()
-							}
-						}
+infix fun <T> Typed<T>.castTo(toType: Type): Term<T>? =
+	if (type == toType) term // TODO: This can quickly become quadratic. Maybe return (Term, didCast)?
+	else resolveLinkOrNull
+		?.let { typedLink ->
+			toType.lineLinkOrNull?.let { typeLink ->
+				typedLink.tail.castTo(typeLink.tail)?.let { castTail ->
+					typedLink.head.castTo(typeLink.head)?.let { castHead ->
+						castTail.plus(castHead)
 					}
 				}
 			}
 		}
+		?: notNullIf(toType.isEmpty) { term }
+
+infix fun <T> TypedLine<T>.castTo(toLine: Line): Term<T>? =
+	when (line) {
+		is NativeLine -> ifOrNull(line == toLine) { term }
+		is ChoiceLine ->
+			if (toLine is ChoiceLine) (term of line.choice).castTo(toLine.choice)
+			else null
+		is ArrowLine -> ifOrNull(line == toLine) { term }
 	}
 
-fun <T> Term<T>.cast(fromLine: Line, toLine: Line): TypedLine<T>? =
-	TODO()
+infix fun <T> TypedChoice<T>.castTo(toChoice: Choice): Term<T>? =
+	choice.onlyFieldOrNull
+		?.let { onlyField -> (term of onlyField).castTo(toChoice) }
+		?: notNullIf(choice == toChoice) { term }
+
+infix fun <T> TypedField<T>.castTo(toChoice: Choice): Term<T>? =
+	toChoice.onlyFieldOrNull
+		?.let { onlyField -> castTo(onlyField) }
+		?: TODO()
+
+infix fun <T> TypedField<T>.castTo(toField: Field): Term<T>? =
+	ifOrNull(field.string == toField.string) {
+		(term of field.rhs).castTo(toField.rhs)
+	}
