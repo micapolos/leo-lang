e1dc926335ee33cc18eb46e9f857641a783ab99b
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-11-11 16:46:51 +0100

    leo13

--- src/main/java/leo14/typed/decompile.kt
+++ src/main/java/leo14/typed/decompile.kt
@@ -1,7 +1,16 @@
 package leo14.typed
 
 import leo14.Script
-import leo14.typed.eval.anyScriptLine
+import leo14.ScriptLine
+import leo14.literal
+
+val Any.anyScriptLine: ScriptLine
+	get() = when (this) {
+		is String -> leo14.line(literal(this))
+		is Int -> leo14.line(literal(this))
+		is Double -> leo14.line(literal(this))
+		else -> leo14.line(literal("any($this)"))
+	}
 
 val Typed<Any>.anyDecompile: Script
 	get() =
