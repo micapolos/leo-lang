693eaa279672f52b66fc5e45413542acab7cb98d
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-11-17 00:08:08 +0100

    leo13

--- src/test/java/leo14/typed/compiler/compilerParserTest.kt
+++ src/test/java/leo14/typed/compiler/compilerParserTest.kt
@@ -1,12 +1,13 @@
 package leo14.typed.compiler
 
 import leo.base.assertEqualTo
+import leo14.typed.typed
 import kotlin.test.Test
 
 class CompilerParserTest {
 	@Test
 	fun test() {
 		compile("zero()")
-			.assertEqualTo(null)
+			.assertEqualTo(typed("zero"))
 	}
 }
