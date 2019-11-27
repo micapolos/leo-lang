6a572e58cb30132b99a7efcd598e73afe4490c4d
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-11-27 23:07:53 +0100

    leo13

--- src/main/java/leo14/typed/compiler/compiledParser.kt
+++ src/main/java/leo14/typed/compiler/compiledParser.kt
@@ -90,7 +90,11 @@ fun <T> CompiledParserParent<T>.end(compiled: Compiled<T>): Compiler<T> =
 	}
 
 fun <T> CompiledParser<T>.next(fn: Compiled<T>.() -> Compiled<T>): Compiler<T> =
-	compiler(updateCompiled(fn).updateCompiled { resolve(phase, context.evaluator) })
+	compiler(updateCompiled(fn).resolvePhase)
+
+val <T> CompiledParser<T>.resolvePhase: CompiledParser<T>
+	get() =
+		updateCompiled { resolve(phase, context.evaluator) }
 
 fun <T> CompiledParser<T>.resolve(line: TypedLine<T>) =
 	copy(compiled = compiled.resolve(line, context).resolve(phase, context.evaluator))
