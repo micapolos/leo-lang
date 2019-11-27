5f4adf9c51b843d4525312aad5deffea3eeae77b
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-11-27 21:52:08 +0100

    leo13

--- src/main/java/leo14/lambda/native.kt
+++ src/main/java/leo14/lambda/native.kt
@@ -3,5 +3,5 @@ package leo14.lambda
 import leo14.native.Native
 import leo14.native.nativeEvaluator
 
-val Term<Native>.nativeValue: Value<Native> get() = nativeEvaluator.evaluate(value(emptyScope(), this))
+val Term<Native>.nativeValue: Value<Native> get() = nativeEvaluator.evaluate(value(scope(), this))
 val Term<Native>.nativeEval: Term<Native> get() = nativeValue.term
--- src/main/java/leo14/lambda/scope.kt
+++ src/main/java/leo14/lambda/scope.kt
@@ -2,13 +2,9 @@ package leo14.lambda
 
 import leo13.*
 
-data class Scope<T>(
-	val valueStack: Stack<Value<T>>,
-	val evaluator: Evaluator<T>)
+data class Scope<T>(val valueStack: Stack<Value<T>>)
 
-fun <T> emptyScope(evaluator: Evaluator<T> = nullEvaluator()): Scope<T> =
-	Scope(stack(), evaluator)
-
-fun <T> Scope<T>.push(value: Value<T>) = Scope(valueStack.push(value), evaluator)
-
+val <T> Stack<Value<T>>.scope get() = Scope(this)
+fun <T> scope(vararg values: Value<T>) = stack(*values).scope
+fun <T> Scope<T>.push(value: Value<T>) = valueStack.push(value).scope
 operator fun <T> Scope<T>.get(index: Index): Value<T>? = valueStack.get(index)
