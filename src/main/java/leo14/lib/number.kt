f28d6ffa98ba7d757d073c48e5d8d49a87dffe22
Michał Pociecha-Łoś <micapolos@gmail.com>
2019-12-11 23:00:53 +0100

    leo13

--- src/main/java/leo14/lib/number.kt
+++ src/main/java/leo14/lib/number.kt
@@ -1,7 +1,6 @@
 package leo14.lib
 
 import leo14.lambda.invoke
-import leo14.lambda.native
 import leo14.lambda.nativeEval
 import leo14.lambda.term
 import leo14.native.*
@@ -10,26 +9,27 @@ import leo14.typed.numberLine
 data class Number(override val term: Term) : Obj() {
 	override val typeLine get() = numberLine
 	override fun toString() = super.toString()
-	val string get() = term.native.string
-	val increment
-		get() =
-			term(numberIncNative).invoke(term).nativeEval.number
-	val decrement
-		get() =
-			term(numberDecNative).invoke(term).nativeEval.number
-
-	operator fun plus(number: Number) =
-		term(numberPlusNumberNative).invoke(term).invoke(number.term).nativeEval.number
-
-	operator fun minus(number: Number) =
-		term(numberMinusNumberNative).invoke(term).invoke(number.term).nativeEval.number
-
-	operator fun times(number: Number) =
-		term(numberTimesNumberNative).invoke(term).invoke(number.term).nativeEval.number
-
-	operator fun unaryMinus() = 0.number - this
 }
 
 val Term.number get() = Number(this)
 val Int.number get() = term(native(this)).number
 val Double.number get() = term(native(this)).number
+
+val Number.increment
+	get() =
+		term(numberIncNative).invoke(term).nativeEval.number
+
+val Number.decrement
+	get() =
+		term(numberDecNative).invoke(term).nativeEval.number
+
+operator fun Number.plus(number: Number) =
+	term(numberPlusNumberNative).invoke(term).invoke(number.term).nativeEval.number
+
+operator fun Number.minus(number: Number) =
+	term(numberMinusNumberNative).invoke(term).invoke(number.term).nativeEval.number
+
+operator fun Number.times(number: Number) =
+	term(numberTimesNumberNative).invoke(term).invoke(number.term).nativeEval.number
+
+operator fun Number.unaryMinus() = 0.number - this
