afa4286d3f69be728b8ac6c61ac437a010f649ce
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-03-22 15:34:16 +0100

    leo14

--- src/main/java/leo14/untyped/dsl2/dsl.kt
+++ src/main/java/leo14/untyped/dsl2/dsl.kt
@@ -2,12 +2,14 @@ package leo14.untyped.dsl2
 
 // TODO: Auto-generate this file
 
+fun X.author(v: V) = x("author", v)
 fun X.decrement(v: V) = x("decrement", v)
 fun X.does(v: V) = x("does", v)
 fun X.given(v: V) = x("given", v)
 fun X.gives(v: V) = x("gives", v)
 fun X.increment(v: V) = x("increment", v)
 fun X._is(v: V) = x("is", v)
+fun X.leonardo(v: V) = x("leonardo", v)
 fun X.minus(v: V) = x("minus", v)
 fun X.my(v: V) = x("my", v)
 fun X.number(v: V) = x("number", v)
@@ -15,6 +17,7 @@ fun X.plus(v: V) = x("plus", v)
 fun X.point(v: V) = x("point", v)
 fun X.print(v: V) = x("print", v)
 fun X.quote(v: V) = x("quote", v)
+fun X.text(v: V) = x("text", v)
 fun X.times(v: V) = x("times", v)
 fun X.unquote(v: V) = x("unquote", v)
 fun X.vector(v: V) = x("vector", v)
@@ -22,12 +25,14 @@ fun X.x(v: V) = x("x", v)
 fun X.y(v: V) = x("y", v)
 fun X.z(v: V) = x("z", v)
 
+val X.author get() = author { this }
 val X.decrement get() = decrement { this }
 val X.does get() = does { this }
 val X.given get() = given { this }
 val X.gives get() = gives { this }
 val X.increment get() = increment { this }
 val X._is get() = _is { this }
+val X.leonardo get() = leonardo { this }
 val X.minus get() = minus { this }
 val X.my get() = my { this }
 val X.number get() = number { this }
@@ -35,6 +40,7 @@ val X.plus get() = plus { this }
 val X.point get() = point { this }
 val X.print get() = print { this }
 val X.quote get() = quote { this }
+val X.text get() = text { this }
 val X.times get() = times { this }
 val X.unquote get() = unquote { this }
 val X.vector get() = vector { this }
---
+++ src/main/java/leo14/untyped/dsl2/example/leonardo.kt
@@ -0,0 +1,11 @@
+package leo14.untyped.dsl2.example
+
+import leo14.untyped.dsl2._run
+import leo14.untyped.dsl2.leonardo
+import leo14.untyped.dsl2.print
+
+fun main() {
+	_run {
+		leonardo.print
+	}
+}
