7c95b8477f2166eff5fd86b8317871190ff65018
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-03-22 15:42:19 +0100

    leo14

--- src/main/java/leo14/untyped/dsl2/core.kt
+++ src/main/java/leo14/untyped/dsl2/core.kt
@@ -8,9 +8,9 @@ import leo14.untyped.write
 var _reader = emptyReader
 
 object X
-typealias V = X.() -> X
+typealias F = X.() -> X
 
 fun X.x(token: Token) = also { _reader = _reader.write(token)!! }
-fun X.x(name: String, v: V) = x(leo14.token(leo14.begin(name))).v().x(leo14.token(end))
+fun X.x(name: String, f: F) = x(leo14.token(leo14.begin(name))).f().x(leo14.token(end))
 fun X.x(name: String) = x(name) { this }
 
