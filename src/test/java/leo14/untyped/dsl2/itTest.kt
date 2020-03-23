67e546a11dfc9e932143bcf20d92a3653df899e0
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-03-23 08:48:54 +0100

    leo14

---
+++ src/test/java/leo14/untyped/dsl2/itTest.kt
@@ -0,0 +1,51 @@
+package leo14.untyped.dsl2
+
+import kotlin.test.Test
+
+class ItTest {
+	@Test
+	fun _test() {
+		_run {
+			assert {
+				zero.it
+				gives { zero.it }
+			}
+
+			assert {
+				zero
+				it { one }
+				gives {
+					zero
+					one
+				}
+			}
+
+			assert {
+				zero
+				it {
+					x
+					y
+				}
+				gives {
+					zero
+					it {
+						x
+						y
+					}
+				}
+			}
+
+			assert {
+				zero
+				it {
+					number(2)
+					plus { number(3) }
+				}
+				gives {
+					zero
+					number(5)
+				}
+			}
+		}
+	}
+}
