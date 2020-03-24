0b38de8a936d70e4497dbc3de1360100df25c5db
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-03-24 16:20:10 +0100

    leo14

---
+++ src/main/java/leo/base/parameter.kt
@@ -0,0 +1,15 @@
+package leo.base
+
+data class Parameter<T>(var value: T)
+
+fun <T> parameter(value: T) = Parameter(value)
+
+fun <T, R> Parameter<T>.run(newValue: T, fn: () -> R) =
+	newValue.let { oldValue ->
+		try {
+			value = newValue
+			fn()
+		} finally {
+			value = oldValue
+		}
+	}
