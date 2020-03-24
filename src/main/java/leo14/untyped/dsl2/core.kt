04b660e93a2b22f600161a476a87affc523b8723
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-03-24 16:16:27 +0100

    leo14

--- src/main/java/leo14/untyped/dsl2/core.kt
+++ src/main/java/leo14/untyped/dsl2/core.kt
@@ -4,12 +4,12 @@ import leo14.Token
 import leo14.untyped.emptyReader
 import leo14.untyped.write
 
-var _reader = emptyReader
+var reader_ = emptyReader
 
 object X
 typealias F = X.() -> X
 
-fun X.x(token: Token) = also { _reader = _reader.write(token)!! }
+fun X.x(token: Token) = also { reader_ = reader_.write(token)!! }
 fun X.x(name: String, f: F) = x(leo14.token(leo14.begin(name))).f().x(leo14.token(leo14.end))
 fun X.x(name: String) = x(name) { this }
 
--- src/main/java/leo14/untyped/dsl2/util.kt
+++ src/main/java/leo14/untyped/dsl2/util.kt
@@ -1,8 +1,16 @@
 package leo14.untyped.dsl2
 
+import leo14.untyped.emptyReader
+
 fun X.number(int: Int) = x(leo14.token(leo14.literal(int)))
 fun X.number(double: Double) = x(leo14.token(leo14.literal(double)))
 fun X.text(string: String) = x(leo14.token(leo14.literal(string)))
 
-fun run_(f: F) = X.f()
-fun library_(f: F) = X.f() // TODO
+fun run_(f: F) =
+	try {
+		X.f()
+	} finally {
+		reader_ = emptyReader
+	}
+
+fun library_(f: F) = run_(f) // TODO
