f3dd76c285827b662575e50f4b7914fc8d224a94
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-03-27 20:52:12 +0100

    leo14

--- src/main/java/leo14/untyped/dsl2/library/core.kt
+++ src/main/java/leo14/untyped/dsl2/library/core.kt
@@ -4,9 +4,40 @@ import leo14.untyped.dsl2.*
 
 val core = library_ {
 	nothing.gives { nothing_ }
+	assert { nothing.gives { nothing_ } }
+
 	anything.clear.gives { nothing }
+	assert { zero.clear.gives { nothing } }
 
 	anything
 	replace { anything }
-	does { given.replace.content }
+	does { given.replace.object_ }
+	assert { zero.replace { one }.gives { one } }
+
+	if_ { anything }
+	then_ { anything }
+	else_ { anything }
+	expands {
+		given.if_.object_
+		match {
+			true_ { given.then_.object_ }
+			false_ { given.else_.object_ }
+		}
+	}
+
+	assert {
+		if_ { number(1).equals_ { number(1) } }
+		then_ { text("ok") }
+		else_ { text("not ok") }
+		gives { text("ok") }
+	}
+
+	assert {
+		if_ { number(1).equals_ { number(2) } }
+		then_ { text("ok") }
+		else_ { text("not ok") }
+		gives { text("not ok") }
+	}
 }
+
+fun main() = run_(core)
