952f09a7a7d8f31872629f6cfdf1cf8098257539
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-04-01 22:31:45 +0200

    leo14

--- src/main/java/leo14/untyped/binding.kt
+++ src/main/java/leo14/untyped/binding.kt
@@ -7,3 +7,21 @@ data class Binding(val key: Thunk, val value: Thunk)
 fun binding(key: Thunk, value: Thunk) = Binding(key, value)
 fun Thunk.bindingTo(value: Thunk) = Binding(this, value)
 fun Binding.apply(thunk: Thunk): Thunk? = notNullIf(key == thunk) { value }
+
+val Thunk.parseBinding: Binding?
+	get() =
+		null
+			?: parseIsBinding
+			?: parseAsBinding
+
+val Thunk.parseIsBinding: Binding?
+	get() =
+		value.sequenceOrNull?.matchInfixOrPrefix(isName) { lhs, rhs ->
+			lhs.bindingTo(rhs)
+		}
+
+val Thunk.parseAsBinding: Binding?
+	get() =
+		matchInfix(asName) { lhs, rhs ->
+			rhs.bindingTo(lhs)
+		}
