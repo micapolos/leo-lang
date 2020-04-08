a72c2e0bb56f6bf03ce3e9ff91a62ba6ab85844a
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-04-08 15:52:56 +0200

    leo14

--- src/main/java/leo14/untyped/typed/compiled.kt
+++ src/main/java/leo14/untyped/typed/compiled.kt
@@ -15,13 +15,17 @@ typealias Erase = () -> Value
 typealias EraseOf<T> = () -> T
 
 data class Compiled(val type: Type, val erase: Erase)
-data class CompiledLink(val link: TypeLink, val erase: Erase)
+data class CompiledLink(val lhs: Compiled, val line: CompiledLine)
 data class CompiledFunction(val function: TypeFunction, val erase: Erase)
 data class CompiledAnything(val erase: Erase)
+data class CompiledLine(val line: TypeLine, val erase: Erase)
+data class CompiledField(val name: String, val rhs: Compiled)
 
 infix fun Type.compiled(erase: Erase) = Compiled(this, erase)
-infix fun TypeLink.compiled(erase: Erase) = CompiledLink(this, erase)
+infix fun Compiled.linkTo(line: CompiledLine) = CompiledLink(this, line)
 infix fun TypeFunction.compiled(erase: Erase) = CompiledFunction(this, erase)
+infix fun TypeLine.compiled(erase: Erase) = CompiledLine(this, erase)
+infix fun String.fieldTo(rhs: Compiled) = CompiledField(this, rhs)
 fun anythingCompiled(erase: Erase) = CompiledAnything(erase)
 
 inline fun <L, R> Compiled.apply(rhs: Compiled, type: Type, crossinline fn: L.(R) -> Value): Compiled =
@@ -102,16 +106,27 @@ fun Compiled.matchAnything(fn: (Erase) -> Compiled?): Compiled? =
 fun Compiled.matchFunction(fn: (TypeFunction, Erase) -> Compiled?): Compiled? =
 	(type as? FunctionType)?.function?.let { function -> fn(function, erase) }
 
+val Compiled.linkOrNull: CompiledLink?
+	get() =
+		(type as? LinkType)?.link?.let { link ->
+			if (link.lhs.isStatic || link.line.isStatic)
+				link.lhs.compiled(erase) linkTo link.line.compiled(erase)
+			else
+				link.lhs.compiled { (erase() as Pair<*, *>).first } linkTo
+					link.line.compiled { (erase() as Pair<*, *>).second }
+		}
+
+val CompiledLine.fieldOrNull: CompiledField?
+	get() =
+		(line as? FieldTypeLine)?.field?.let { field ->
+			field.name fieldTo field.rhs.compiled(erase)
+		}
+
 fun Compiled.matchInfix(name: String, fn: Compiled.(Compiled) -> Compiled?): Compiled? =
-	(type as? LinkType)?.link?.let { link ->
-		(link.line as? FieldTypeLine)?.field?.let { field ->
+	linkOrNull?.let { link ->
+		link.line.fieldOrNull?.let { field ->
 			ifOrNull(field.name == name) {
-				if (link.lhs.isStatic || field.rhs.isStatic)
-					fn.invoke(link.lhs.compiled(erase), field.rhs.compiled(erase))
-				else
-					fn.invoke(
-						link.lhs.compiled { (value as Pair<*, *>).first },
-						field.rhs.compiled { (value as Pair<*, *>).second })
+				link.lhs.fn(field.rhs)
 			}
 		}
 	}
