7dcd621cfeaacf4b0d4d47adaa7cd29b3c90b022
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-04-12 21:24:27 +0200

    leo14 typed

--- src/main/java/leo14/untyped/typed/lambda/sandbox/sandbox.kt
+++ src/main/java/leo14/untyped/typed/lambda/sandbox/sandbox.kt
@@ -1,10 +1,10 @@
 package leo14.untyped.typed.lambda.sandbox
 
 import leo14.untyped.dsl2.main_
-import leo14.untyped.dsl2.nothing
+import leo14.untyped.dsl2.nothing_
 
 fun main() {
 	main_ {
-		nothing
+		nothing_
 	}
 }
