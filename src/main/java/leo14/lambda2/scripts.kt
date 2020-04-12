f06c1359c7d55678ea7ed07a3021e73af001f660
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-04-12 19:47:49 +0200

    leo14 typed

--- src/main/java/leo14/lambda2/scripts.kt
+++ src/main/java/leo14/lambda2/scripts.kt
@@ -8,6 +8,9 @@ import leo14.script
 val Term.script: Script
 	get() =
 		when (this) {
+			pair -> script("#pair")
+			first -> script("#first")
+			second -> script("#second")
 			is ValueTerm -> script("value" lineTo script(value.toString()))
 			is AbstractionTerm -> script("lambda" lineTo body.script)
 			is ApplicationTerm -> lhs.script.plus("apply" lineTo rhs.script)
