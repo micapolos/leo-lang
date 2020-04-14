8d5e88895e84f673e631b548422988d9995b7a61
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-04-14 13:50:59 +0200

    leo14 typed

--- src/main/java/leo14/lambda2/scripts.kt
+++ src/main/java/leo14/lambda2/scripts.kt
@@ -6,13 +6,14 @@ val Term.script: Script
 	get() =
 		null
 			?: pairScriptOrNull
+			//?: bindScriptOrNull
 			?: when (this) {
 				id -> script("id")
 				pair -> script("pair")
 				first -> script("first")
 				second -> script("second")
 				is ValueTerm -> script("value" lineTo script(value.toString()))
-				is AbstractionTerm -> script("lambda" lineTo body.script)
+				is AbstractionTerm -> lambdaScript
 				is ApplicationTerm -> lhs.script.plus("invoke" lineTo rhs.script)
 				is IndexTerm -> script("get" lineTo script(literal(index)))
 			}
@@ -25,3 +26,24 @@ val Term.pairScriptOrNull: Script?
 					"first" lineTo pair.first.script,
 					"second" lineTo pair.second.script))
 		}
+
+val AbstractionTerm.lambdaScript: Script
+	get() =
+		script("lambda" lineTo body.script)
+
+//val Term.bindScriptOrNull: Script?
+//	get() =
+//		bindScriptOrNull(stack())
+//
+//fun Term.bindScriptOrNull(args: Stack<Term>): Script? =
+//	when (this) {
+//		is ValueTerm -> null
+//		is AbstractionTerm -> when (args) {
+//			is EmptyStack -> lambdaScript
+//			is LinkStack -> body.bindScriptOrNull(args.link.stack)?.let { rhsScript ->
+//				script("bind" lineTo args.link.value.script).plus(rhsScript)
+//			}
+//		}
+//		is ApplicationTerm -> lhs.bindScriptOrNull(args.push(rhs))
+//		is IndexTerm -> null
+//	}
---
+++ src/test/java/leo14/lambda2/scriptTest.kt
@@ -0,0 +1,16 @@
+package leo14.lambda2
+
+class ScriptTest {
+//	@Test
+//	fun bind() {
+//		fn(fn("plus".valueTerm))
+//			.invoke(10.valueTerm)
+//			.invoke(20.valueTerm)
+//			.script
+//			.assertEqualTo(
+//				leo(
+//					"bind" lineTo 10.valueTerm.script,
+//					"bind" lineTo 20.valueTerm.script,
+//					"apply" lineTo "plus".valueTerm.script))
+//	}
+}
