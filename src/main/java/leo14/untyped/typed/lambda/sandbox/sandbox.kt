3be04e0216338fa2346b277209352d9726007ac3
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-04-15 00:22:23 +0200

    leo14 typed

--- src/main/java/leo14/untyped/typed/lambda/sandbox/sandbox.kt
+++ src/main/java/leo14/untyped/typed/lambda/sandbox/sandbox.kt
@@ -3,10 +3,9 @@ package leo14.untyped.typed.lambda.sandbox
 import leo14.untyped.dsl2.equals_
 import leo14.untyped.dsl2.main_
 import leo14.untyped.dsl2.number
-import leo14.untyped.dsl2.text
 
 fun main() {
 	main_ {
-		number(10).equals_ { text("foo") }
+		number(10).equals_ { number(20) }
 	}
 }
