3c92e337620fe1ddc76804ac72a29877c7e7c280
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-04-17 00:56:58 +0200

    leo15

--- src/main/java/leo15/type/expression.kt
+++ src/main/java/leo15/type/expression.kt
@@ -2,11 +2,7 @@ package leo15.type
 
 import leo15.lambda.Term
 
-import leo15.type.ExpressionKind.CONSTANT
-import leo15.type.ExpressionKind.DYNAMIC
+data class Expression(val term: Term, val isConstant: Boolean)
 
-enum class ExpressionKind { CONSTANT, DYNAMIC }
-data class Expression(val term: Term, val kind: ExpressionKind)
-
-val Term.constantExpression get() = Expression(this, CONSTANT)
-val Term.dynamicExpression get() = Expression(this, DYNAMIC)
+val Term.constantExpression get() = Expression(this, isConstant = true)
+val Term.dynamicExpression get() = Expression(this, isConstant = false)
