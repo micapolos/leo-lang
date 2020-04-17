436e1c4a5235604a314b763b463769f3200a7bfe
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-04-17 18:01:30 +0200

    leo15

--- src/main/java/leo15/type/cast.kt
+++ src/main/java/leo15/type/cast.kt
@@ -1,16 +1,95 @@
 package leo15.type
 
-import leo.base.Parameter
-import leo.base.notNullIf
-import leo.base.parameter
+import leo.base.*
+import leo15.lambda.choiceTerm
 
 val castRecursiveParameter: Parameter<Recursive?> = parameter(null)
 
-fun Typed.castExpression(to: Type): Expression? =
+fun Typed.cast(to: Type): Typed? =
 	when (to) {
-		EmptyType -> notNullIf(type.isEmpty) { expression }
-		is LinkType -> TODO()
-		is RepeatingType -> TODO()
-		is RecursiveType -> TODO()
-		RecurseType -> TODO()
+		EmptyType -> notNullIf(isEmpty) { this }
+		is LinkType -> linkOrNull?.cast(to.link)
+		is RepeatingType -> cast(to.repeating)
+		is RecursiveType -> cast(to.recursive)
+		RecurseType -> castRecurse
 	}
+
+fun TypedLink.cast(to: TypeLink): Typed? =
+	lhs.cast(to.lhs).orNullApply(choice.cast(to.choice), Typed::plus)
+
+fun TypedChoice.cast(to: Choice): TypedChoice? =
+	onlyLineOrNull
+		?.run { cast(to) }
+		?: exactCast(to)
+
+fun TypedLine.cast(to: Choice): TypedChoice? =
+	when (to) {
+		is LineChoice -> notNullIf(typeLine == to.line) { choice }
+		is LinkChoice -> cast(to.link, to, to.lineCount, 0)
+	}
+
+fun TypedLine.cast(to: Choice, choice: Choice, size: Int, index: Int): TypedChoice? =
+	when (to) {
+		is LineChoice ->
+			cast(to.line)
+				?.let { choiceTerm(size, index, it.expression.term).dynamicExpression.of(choice) }
+		is LinkChoice -> cast(to.link, choice, size, index)
+	}
+
+fun TypedLine.cast(to: ChoiceLink, choice: Choice, size: Int, index: Int): TypedChoice? =
+	cast(to.line)
+		?.let { choiceTerm(size, index, it.expression.term).dynamicExpression.of(choice) }
+		?: cast(to.lhs, choice, size, index.inc())
+
+fun TypedChoice.exactCast(to: Choice): TypedChoice? =
+	notNullIf(choice == to) { this }
+
+fun TypedLine.cast(to: TypeLine): TypedLine? =
+	when (to) {
+		is LiteralTypeLine -> TODO()
+		is FieldTypeLine -> fieldOrNull?.cast(to.field)
+		is ArrowTypeLine -> arrowOrNull?.cast(to.arrow)
+		JavaTypeLine -> notNullIf(isJava) { this }
+	}
+
+fun TypedField.cast(to: TypeField): TypedLine? =
+	ifOrNull(field.name == to.name) {
+		to.name.applyOrNull(rhs.cast(to.rhs)) { rhs ->
+			fieldTo(rhs).line
+		}
+	}
+
+fun TypedArrow.cast(to: Arrow): TypedLine? =
+	notNullIf(arrow == to) { line }
+
+fun Typed.cast(to: Repeating): Typed? =
+	repeatingOrNull.let { repeating ->
+		if (repeating == null) castRepeating(to)
+		else repeating.cast(to)
+	}?.typed
+
+fun TypedRepeating.cast(to: Repeating): TypedRepeating? =
+	notNullIf(repeating.choice == to.choice) { this }
+
+fun Typed.castRepeating(to: Repeating): TypedRepeating? =
+	when (type) {
+		EmptyType -> expression.of(to)
+		is LinkType -> castRepeating(to)
+		is RepeatingType -> null
+		is RecursiveType -> null
+		RecurseType -> null
+	}
+
+fun TypedLink.castRepeating(to: Repeating): TypedRepeating? =
+	choice.cast(to.choice)?.let { cast ->
+		lhs.castRepeating(to)?.plus(cast)
+	}
+
+fun Typed.cast(to: Recursive): Typed? =
+	type.recursiveOrNull
+		?.let { TODO() }
+		?: TODO()
+
+val Typed.castRecurse: Typed?
+	get() =
+		cast(castRecursiveParameter.value!!.type)
--- src/main/java/leo15/type/typed.kt
+++ src/main/java/leo15/type/typed.kt
@@ -6,6 +6,7 @@ import leo.base.notNullIf
 import leo13.Stack
 import leo14.*
 import leo15.*
+import leo15.lambda.append
 import leo15.lambda.valueTerm
 
 data class Typed(val expression: Expression, val type: Type) {
@@ -16,7 +17,8 @@ data class TypedChoice(val expression: Expression, val choice: Choice)
 data class TypedLine(val expression: Expression, val typeLine: TypeLine)
 data class TypedField(val expression: Expression, val field: TypeField)
 data class TypedLink(val lhs: Typed, val choice: TypedChoice)
-data class TypedArrow(val expression: Expression, val function: Arrow)
+data class TypedArrow(val expression: Expression, val arrow: Arrow)
+data class TypedRepeating(val expression: Expression, val repeating: Repeating)
 
 data class Match(val typeLineStack: Stack<TypeLine>, val expression: Expression, val typeOrNull: Type?)
 data class Case(val name: String, val typed: Typed)
@@ -30,6 +32,7 @@ infix fun Expression.of(choice: Choice) = TypedChoice(this, choice)
 infix fun Expression.of(line: TypeLine) = TypedLine(this, line)
 infix fun Expression.of(field: TypeField) = TypedField(this, field)
 infix fun Expression.of(arrow: Arrow) = TypedArrow(this, arrow)
+infix fun Expression.of(repeating: Repeating) = TypedRepeating(this, repeating)
 infix fun Typed.linkTo(choice: TypedChoice) = TypedLink(this, choice)
 
 val emptyTyped: Typed = emptyExpression of emptyType
@@ -50,6 +53,11 @@ val Expression.javaTextTyped: Typed get() = typed(textName lineTo of(javaType))
 fun Typed.plus(typed: TypedChoice): Typed =
 	add(expression, type.isStatic, typed.expression, typed.choice.isStatic) of type.plus(typed.choice)
 
+fun TypedRepeating.plus(typed: TypedChoice): TypedRepeating? =
+	notNullIf(repeating.choice == typed.choice) {
+		expression.term.append(typed.expression.term).dynamicExpression.of(repeating)
+	}
+
 fun Typed.plus(typed: TypedLine): Typed = plus(typed.choice)
 fun Typed.plus(name: String): Typed = plus(name lineTo emptyTyped)
 
@@ -63,16 +71,21 @@ val Typed.linkOrNull: TypedLink?
 
 val TypedLink.typed: Typed get() = lhs.plus(choice)
 
+infix fun String.fieldTo(typed: Typed): TypedField =
+	typed.expression of fieldTo(typed.type)
+
 infix fun String.lineTo(typed: Typed): TypedLine =
 	typed.expression of lineTo(typed.type)
 
 val TypedField.rhs: Typed get() = expression.of(field.rhs)
 val TypedLine.fieldOrNull: TypedField? get() = typeLine.fieldOrNull?.let { expression.of(it) }
 val TypedLine.arrowOrNull: TypedArrow? get() = typeLine.arrowOrNull?.let { expression.of(it) }
+val TypedLine.isJava: Boolean get() = typeLine.isJava
 val TypedLine.rhsOrNull: Typed? get() = fieldOrNull?.rhs
 val Typed.onlyChoiceOrNull: TypedChoice?
 	get() =
 		linkOrNull?.run { notNullIf(lhs.type.isEmpty) { choice } }
+val Typed.repeatingOrNull: TypedRepeating? get() = expression.applyOrNull(type.repeatingOrNull) { of(it) }
 val Typed.onlyLineOrNull: TypedLine? get() = onlyChoiceOrNull?.onlyLineOrNull
 val Typed.rhsOrNull: Typed? get() = onlyLineOrNull?.fieldOrNull?.rhs
 val TypedChoice.onlyLineOrNull: TypedLine?
@@ -83,6 +96,9 @@ val TypedChoice.onlyLineOrNull: TypedLine?
 val TypedLine.choice: TypedChoice
 	get() =
 		expression of typeLine.choice
+val TypedArrow.line: TypedLine get() = expression.of(arrow.line)
+val TypedField.line: TypedLine get() = expression.of(field.line)
+val TypedRepeating.typed: Typed get() = expression.of(repeating.toType)
 
 fun typed(line: TypedLine, vararg lines: TypedLine): Typed =
 	emptyTyped.plus(line).fold(lines) { plus(it) }
