534c1bf83daeac1816b3971600f79dea7279c6a0
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-04-27 22:30:44 +0200

    leo15

--- src/main/java/leo16/unsafe.kt
+++ src/main/java/leo16/unsafe.kt
@@ -1,19 +1,19 @@
 package leo16
 
 import leo.base.notNullIf
-import leo13.linkOrNull
-import leo13.mapFirst
-import leo13.onlyOrNull
-import leo13.stack
+import leo13.*
+import leo15.lastName
 
 data class Case(val selectedWord: String, val fn: Script.() -> Script)
 
 fun String.gives(fn: Script.() -> Script) = Case(this, fn)
 
+val Script.thing: Script
+	get() =
+		sentenceStack.onlyOrNull!!.followingScript
+
 infix fun Script.get(word: String): Script =
-	sentenceStack
-		.onlyOrNull!!
-		.followingScript
+	thing
 		.sentenceStack
 		.mapFirst {
 			notNullIf(firstWord == word) {
@@ -24,17 +24,17 @@ infix fun Script.get(word: String): Script =
 infix fun Script.make(word: String): Script =
 	script(word(this))
 
-val Script.thing: Script
+val Script.last: Script
 	get() =
-		sentenceStack.onlyOrNull!!.followingScript
+		script(lastName(thing.sentenceStack.linkOrNull!!.value))
 
-val Script.tail: Script
+val Script.previous: Script
 	get() =
-		sentenceStack.linkOrNull!!.stack.script
+		sentenceStack.onlyOrNull!!.run {
+			script(previousName(firstWord(followingScript.sentenceStack.linkOrNull!!.stack.script)))
+		}
 
-val Script.head: Script
-	get() =
-		script(sentenceStack.linkOrNull!!.value)
+val Script.listIsEmpty: Boolean get() = thing.sentenceStack.isEmpty
 
 fun Script.match(vararg cases: Case): Script =
 	sentenceStack
--- src/test/java/leo16/unsafeTest.kt
+++ src/test/java/leo16/unsafeTest.kt
@@ -1,10 +1,81 @@
 package leo16
 
 import leo.base.assertEqualTo
+import leo15.lastName
+import leo15.previousName
 import kotlin.test.Test
 import kotlin.test.assertFails
 
 class UnsafeTest {
+	@Test
+	fun thing() {
+		script("point"()).thing.assertEqualTo(script())
+
+		script(
+			"point"(
+				"x"("zero"()),
+				"y"("one"())))
+			.thing
+			.assertEqualTo(
+				script(
+					"x"("zero"()),
+					"y"("one"())))
+
+		assertFails { script().thing }
+		assertFails { script("x"("zero"()), "y"("one"())).thing }
+	}
+
+	@Test
+	fun get() {
+		script(
+			"circle"(
+				"radius"("ten"()),
+				"center"("point"(
+					"x"("zero"()),
+					"y"("one"())))))
+			.run {
+				get("radius")
+					.assertEqualTo(script("radius"("ten"())))
+				get("center")
+					.assertEqualTo(
+						script(
+							"center"("point"(
+								"x"("zero"()),
+								"y"("one"())))))
+				assertFails { get("color") }
+			}
+	}
+
+	@Test
+	fun make() {
+		script(
+			"x"("zero"()),
+			"y"("one"()))
+			.make("point")
+			.assertEqualTo(
+				script(
+					"point"(
+						"x"("zero"()),
+						"y"("one"()))))
+	}
+
+	@Test
+	fun list() {
+		script("items"("zero"(), "one"(), "two"()))
+			.run {
+				listIsEmpty.assertEqualTo(false)
+				last.assertEqualTo(script(lastName("two"())))
+				previous.assertEqualTo(script(previousName("items"("zero"(), "one"()))))
+			}
+
+		script("items"())
+			.run {
+				listIsEmpty.assertEqualTo(true)
+				assertFails { last }
+				assertFails { previous }
+			}
+	}
+
 	@Test
 	fun match() {
 		script("shape"("circle"("radius"("zero"()))))
