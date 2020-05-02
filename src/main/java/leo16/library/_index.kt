0bcaf2c1336aa1bb3d32b4f81e28cc1734723587
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-05-02 14:23:29 +0200

    leo16

--- src/main/java/leo16/library/_index.kt
+++ src/main/java/leo16/library/_index.kt
@@ -4,13 +4,11 @@ import leo15.baseName
 import leo15.bitName
 import leo15.listName
 import leo15.pingName
-import leo16.invoke
 import leo16.pattern
-import leo16.value
 
 val valueFunMap = mapOf(
-	value(pingName(value())).pattern to { ping },
-	value(baseName(value())).pattern to { base },
-	value(bitName(value())).pattern to { bit },
-	value(listName(value())).pattern to { list }
+	pingName.pattern to { ping },
+	baseName.pattern to { base },
+	bitName.pattern to { bit },
+	listName.pattern to { list }
 )
