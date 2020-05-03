1c1342d33bb4a003ac0b263324fdea2178f507fe
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-05-03 12:16:40 +0200

    leo16

--- src/test/java/leo16/evalTest.kt
+++ src/test/java/leo16/evalTest.kt
@@ -44,6 +44,7 @@ class EvalTest {
 		evaluate_ { quote { zero.negate } }.assertGives { zero.negate }
 		evaluate_ { quote { zero.is_ { one } } }.assertGives { zero.is_ { one } }
 		evaluate_ { zero.quote { one.two } }.assertGives { zero.one.two }
+		evaluate_ { zero.one.quote { two } }.assertGives { one { zero }.two }
 	}
 
 	@Test
