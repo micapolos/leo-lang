c865dd48e9b4f4b076d85c4ea00805b6703c7d7e
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-05-05 20:33:46 +0200

    leo16

--- src/main/java/leo16/sandbox/sandbox.kt
+++ src/main/java/leo16/sandbox/sandbox.kt
@@ -4,5 +4,13 @@ import leo15.dsl.*
 import leo16.leo_
 
 fun main() = leo_ {
-	"http://mwiacek.com".text.url.get.length
+	0.number
+	fold {
+		"http://mwiacek.com".text.url.get.lines
+		map { giving { given.text.length } }
+		giving {
+			given.folded.number
+			plus { given.next.number }
+		}
+	}
 }
