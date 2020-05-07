62f91e53752325c7f8c426a106aed2fd81c52809
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-05-07 11:15:36 +0200

    leo16

--- src/main/java/leo16/library/url.kt
+++ src/main/java/leo16/library/url.kt
@@ -3,8 +3,13 @@ package leo16.library
 import leo15.dsl.*
 import leo16.dictionary_
 
+fun main() {
+	url
+}
+
 val url = dictionary_ {
 	reflection.import
+	list.import
 
 	import {
 		dictionary {
@@ -26,6 +31,41 @@ val url = dictionary_ {
 				}
 			}
 
+			url.get.protocol.method.is_ {
+				url.class_.method {
+					name { "getProtocol".text }
+					parameter { empty.list }
+				}
+			}
+
+			url.get.host.method.is_ {
+				url.class_.method {
+					name { "getHost".text }
+					parameter { empty.list }
+				}
+			}
+
+			url.get.port.method.is_ {
+				url.class_.method {
+					name { "getPort".text }
+					parameter { empty.list }
+				}
+			}
+
+			url.get.path.method.is_ {
+				url.class_.method {
+					name { "getPath".text }
+					parameter { empty.list }
+				}
+			}
+
+			url.get.query.method.is_ {
+				url.class_.method {
+					name { "getQuery".text }
+					parameter { empty.list }
+				}
+			}
+
 			url.get.method.is_ {
 				url.kotlin.class_
 				method {
@@ -43,10 +83,54 @@ val url = dictionary_ {
 		url
 	}
 
-	any.url.get
+	any.native.url.get
 	gives {
 		url.get.method
 		invoke { parameter { list { item { get.url.native } } } }
 		text.get
+	}
+
+	any.native.url.protocol.gives {
+		protocol.url.native.invoke {
+			method { url.get.protocol }
+			parameter { empty.list }
+		}.text.protocol
+	}
+
+	any.native.url.host.gives {
+		host.url.native.invoke {
+			method { url.get.host }
+			parameter { empty.list }
+		}.text.host
+	}
+
+	any.native.url.path.gives {
+		path.url.native.invoke {
+			method { url.get.path }
+			parameter { empty.list }
+		}.text.path
+	}
+
+	test { "http://mwiacek.com".text.url.protocol gives { "http".text.protocol } }
+	test { "http://mwiacek.com".text.url.host gives { "mwiacek.com".text.host } }
+	test { "http://mwiacek.com".text.url.path gives { "".text.path } }
+	test { "http://mwiacek.com/index.html".text.url.path gives { "/index.html".text.path } }
+
+	any.native.url.reflect.gives {
+		url {
+			this_ { reflect.url.protocol }
+			this_ { reflect.url.host }
+			this_ { reflect.url.path }
+		}
+	}
+
+	test {
+		"http://mwiacek.com/index.html".text.url.reflect.gives {
+			url {
+				protocol { "http".text }
+				host { "mwiacek.com".text }
+				path { "/index.html".text }
+			}
+		}
 	}
 }
