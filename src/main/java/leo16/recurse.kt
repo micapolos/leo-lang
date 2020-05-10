089794d6ee1a21a002ebcd60ebe094efcd34b621
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-05-10 11:57:41 +0200

    recurse

---
+++ src/main/java/leo16/recurse.kt
@@ -0,0 +1,18 @@
+package leo16
+
+import leo16.names.*
+
+data class Recurse(val gives: Gives) {
+	override fun toString() = asField.toString()
+}
+
+val Recurse.asField get() = _recursing(gives.asValue)
+val Gives.recurse get() = Recurse(this)
+
+fun Recurse.take(arg: Value): Value =
+	gives.pattern
+		.gives(gives.function.dictionary
+			.plus(_recurse(_any()).value.pattern.definitionTo(gives.function.body))
+			.function(gives.function.bodyValue))
+		.take(arg)
+		?: _recurse(arg).value
