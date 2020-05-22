7d3537f3ea62e01a3915a15e5fe86dd1eda8d1fa
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-05-22 11:11:06 +0200

    approximate.math

---
+++ src/main/java/leo16/library/math/approximate.kt
@@ -0,0 +1,13 @@
+package leo16.library.math
+
+import leo.base.print
+import leo15.dsl.*
+import leo16.compile_
+
+fun main() {
+	approximate.value.print
+}
+
+val approximate = compile_ {
+	nothing_
+}
