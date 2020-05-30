cefc85d2ef6232e82f8df6a2cdefc2353b31652e
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-05-30 16:00:02 +0200

    lambda.library

--- src/main/java/leo16/library/lambda.kt
+++ src/main/java/leo16/library/lambda.kt
@@ -2,9 +2,6 @@ package leo16.library
 
 import leo.base.print
 import leo15.dsl.*
-import leo15.lambda.at
-import leo15.lambda.invoke
-import leo15.lambda.valueTerm
 import leo16.compile_
 
 fun main() {
@@ -12,63 +9,18 @@ fun main() {
 }
 
 val lambda = compile_ {
-	use { reflection }
-	use { lambda.native }
+	use { number }
 
-	native.number.at
-	does {
-		index.term.constructor
-		invoke { parameter { list { item { at.number.int.native } } } }
+	term.any.is_ {
+		value { anything }
+		or { at { number.any } }
+		or { lambda { term.repeating } }
+		or { term.repeating.apply { term.repeating } }
+		term
 	}
 
-	test { 3.number.at.as_ { text }.equals_ { at(3).nativeText } }
-
-	native.lambda
-	does {
-		abstraction.term.constructor
-		invoke { parameter { list { item { lambda.native } } } }
-	}
-
-	test { 3.number.at.lambda.as_ { text }.equals_ { leo15.lambda.fn(at(3)).nativeText } }
-
-	native
-	apply { native }
-	does {
-		application.term.constructor
-		invoke {
-			parameter {
-				list {
-					item { native }
-					item { apply.native }
-				}
-			}
-		}
-	}
-
-	test {
-		3.number.at.apply { 4.number.at }.as_ { text }
-			.equals_ { at(3).invoke(at(4)).nativeText }
-	}
-
-	native.value
-	does {
-		meta { value }.term.constructor
-		invoke { parameter { list { item { value.native } } } }
-	}
-
-	test { "foo".text.native.value.as_ { text }.equals_ { "foo".valueTerm.nativeText } }
-
-	native.eval
-	does {
-		term.eval.method
-		invoke { parameter { list { item { eval.native } } } }
-	}
-
-//	test {
-//		0.number.at.lambda
-//		apply { "foo".text.native.value }
-//		eval
-//		as_ { text }
-//		equals_ { "foo".text.native.value.nativeText }
-//	}
+	test { term.any.matches { zero.value.term } }
+	test { term.any.matches { 2.number.at.term } }
+	test { term.any.matches { zero.value.term.lambda.term } }
+	test { term.any.matches { zero.value.term.apply { one.value.term }.term } }
 }
