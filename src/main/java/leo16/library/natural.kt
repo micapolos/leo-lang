1dc2e8749c79319005da0a8f3daaa76b98d58e4d
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-06-01 12:49:44 +0200

    natural.library

--- src/main/java/leo16/library/natural.kt
+++ src/main/java/leo16/library/natural.kt
@@ -8,6 +8,8 @@ fun main() {
 }
 
 val natural = dsl_ {
+	use { leo }
+
 	natural.any.is_ {
 		do_ {
 			zero.natural
@@ -18,4 +20,38 @@ val natural = dsl_ {
 	test { natural.any.matches { zero.natural } }
 	test { natural.any.matches { zero.natural.next } }
 	test { natural.any.matches { zero.natural.next.next } }
+
+	natural.any.previous
+	does {
+		previous.thing.equals_ { zero.natural }
+		match {
+			true_ { previous }
+			false_ { previous.thing.thing }
+		}
+	}
+
+	test { zero.natural.previous.equals_ { zero.natural.previous } }
+	test { zero.natural.next.previous.equals_ { zero.natural } }
+	test { zero.natural.next.next.previous.equals_ { zero.natural.next } }
+
+	natural.any
+	plus { natural.any }
+	does {
+		lhs.is_ { thing.tail }
+		rhs.is_ { plus.thing }
+		rhs.equals_ { zero.natural }
+		match {
+			true_ { lhs }
+			false_ {
+				lhs.next
+				plus { rhs.previous }
+				repeat
+			}
+		}
+	}
+
+	test { zero.natural.plus { zero.natural }.equals_ { zero.natural } }
+	test { zero.natural.next.plus { zero.natural }.equals_ { zero.natural.next } }
+	test { zero.natural.plus { zero.natural.next }.equals_ { zero.natural.next } }
+	test { zero.natural.next.plus { zero.natural.next }.equals_ { zero.natural.next.next } }
 }
