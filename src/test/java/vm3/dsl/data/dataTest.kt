42750f3e3c498f571a4c1927c95c5847cadc1713
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-09-18 22:30:37 +0200

    vm3

--- src/test/java/vm3/dsl/data/dataTest.kt
+++ src/test/java/vm3/dsl/data/dataTest.kt
@@ -8,7 +8,7 @@ class DataTest {
 		false.bool
 		10.i32
 		10f.f32
-		array(10.i32)
+		array(10.i32, 20.i32)
 		struct("x" to 10.i32, "y" to 20.i32)
 	}
 }
--- src/test/java/vm3/dsl/value/valueTest.kt
+++ src/test/java/vm3/dsl/value/valueTest.kt
@@ -9,6 +9,8 @@ class ValueTest {
 		10.i32
 		10f.f32
 		array(10.i32, 20.i32)
+		array(10.i32, 20.i32)[0.i32]
 		struct("x" to 10.i32, "y" to 20.i32)
+		struct("x" to 10.i32, "y" to 20.i32)["x"]
 	}
 }
