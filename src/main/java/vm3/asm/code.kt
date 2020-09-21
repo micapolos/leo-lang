b176660cbc7883cfc2b6bc0d2d10f18fc392ec4e
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-09-21 14:22:22 +0200

    vm3

---
+++ src/main/java/vm3/asm/code.kt
@@ -0,0 +1,41 @@
+package vm3.asm
+
+import vm3.float
+
+val List<Op>.code
+	get() =
+		mapIndexed { index, op -> "$index: ${op.code}" }.joinToString("\n")
+
+val Op.code: String
+	get() =
+		when (this) {
+			Op.Exit -> "exit"
+			Op.Nop -> "nop"
+			Op.SysCall -> "syscall"
+			is Op.Jump -> "jump $index"
+			is Op.Call -> "call $jumpIndex ret $retIndex"
+			is Op.Ret -> "ret $index"
+			is Op.Set32 -> assignCode(dst, code32(src))
+			is Op.Copy32 -> assignCode(dst, memCode(src))
+			is Op.CopyIndirect32 -> assignCode(dst, memCode(memCode(src)))
+			is Op.CopyBlock -> assignCode(blockCode(dst, size), blockCode(src, size))
+			is Op.I32Inc -> opCode(dst, "i32.inc", src)
+			is Op.I32Dec -> opCode(dst, "i32.dec", src)
+			is Op.I32Add -> opCode(dst, lhs, "i32.plus", rhs)
+			is Op.I32Sub -> opCode(dst, lhs, "i32.minus", rhs)
+			is Op.I32Mul -> opCode(dst, lhs, "i32.times", rhs)
+			is Op.F32Add -> opCode(dst, lhs, "f32.plus", rhs)
+			is Op.F32Sub -> opCode(dst, lhs, "f32.minus", rhs)
+			is Op.F32Mul -> opCode(dst, lhs, "f32.times", rhs)
+		}
+
+fun code32(int: Int) = "$int (float: ${int.float})"
+fun memCode(addr: Int) = "[$addr]"
+fun memCode(code: String) = "[$code]"
+fun blockCode(index: Int, size: Int) = "[$index:$size]"
+fun prefixCode(op: String, rhs: String) = "$op $rhs"
+fun infixCode(lhs: String, op: String, rhs: String) = "$lhs $op $rhs"
+fun assignCode(lhs: String, rhs: String) = infixCode(lhs, ":=", rhs)
+fun assignCode(lhs: Int, rhs: String) = assignCode(memCode(lhs), rhs)
+fun opCode(dst: Int, op: String, src: Int) = assignCode(dst, prefixCode(op, memCode(src)))
+fun opCode(dst: Int, lhs: Int, op: String, rhs: Int) = assignCode(dst, infixCode(memCode(lhs), op, memCode(rhs)))
---
+++ src/test/java/vm3/asm/codeTest.kt
@@ -0,0 +1,55 @@
+package vm3.asm
+
+import leo.base.assertEqualTo
+import org.junit.Test
+
+class CodeTest {
+	@Test
+	fun code() {
+		listOf(
+			Op.Exit,
+			Op.Nop,
+			Op.SysCall,
+
+			Op.Jump(1),
+			Op.Call(1, 2),
+			Op.Ret(1),
+
+			Op.Set32(1, 2),
+			Op.Copy32(1, 2),
+			Op.CopyIndirect32(1, 2),
+			Op.CopyBlock(1, 2, 128),
+
+			Op.I32Inc(1, 2),
+			Op.I32Dec(1, 2),
+
+			Op.I32Add(1, 2, 3),
+			Op.I32Sub(1, 2, 3),
+			Op.I32Mul(1, 2, 3),
+
+			Op.F32Add(1, 2, 3),
+			Op.F32Sub(1, 2, 3),
+			Op.F32Mul(1, 2, 3))
+			.code
+			.assertEqualTo("""
+				0: exit
+				1: nop
+				2: syscall
+				3: jump 1
+				4: call 1 ret 2
+				5: ret 1
+				6: [1] := 2 (float: 2.8E-45)
+				7: [1] := [2]
+				8: [1] := [[2]]
+				9: [1:128] := [2:128]
+				10: [1] := i32.inc [2]
+				11: [1] := i32.dec [2]
+				12: [1] := [2] i32.plus [3]
+				13: [1] := [2] i32.minus [3]
+				14: [1] := [2] i32.times [3]
+				15: [1] := [2] f32.plus [3]
+				16: [1] := [2] f32.minus [3]
+				17: [1] := [2] f32.times [3]
+			""".trimIndent())
+	}
+}
