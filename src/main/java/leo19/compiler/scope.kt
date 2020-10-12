92a5601c505703667e875c2bb98a7ae6ecf20aea
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-10-12 11:28:29 +0200

    leo19

--- src/main/java/leo19/compiler/scope.kt
+++ src/main/java/leo19/compiler/scope.kt
@@ -1,22 +1,18 @@
 package leo19.compiler
 
 import leo.base.indexed
-import leo.base.mapFirst
 import leo.base.mapFirstOrNull
 import leo.base.notNullIf
 import leo.base.runIf
 import leo13.Stack
-import leo13.firstIndexed
 import leo13.push
 import leo13.seq
 import leo13.stack
 import leo14.Script
 import leo19.term.invoke
-import leo19.term.nullTerm
 import leo19.term.term
 import leo19.term.variable
 import leo19.type.Arrow
-import leo19.type.isStatic
 import leo19.typed.Typed
 import leo19.typed.nullTyped
 import leo19.typed.of
@@ -44,3 +40,6 @@ fun Scope.resolveOrNull(typed: Typed): Typed? =
 			}
 		}
 	}
+
+fun Scope.resolve(typed: Typed): Typed =
+	resolveOrNull(typed) ?: typed
