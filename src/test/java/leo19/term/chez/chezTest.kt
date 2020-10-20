b556dcb4fc3a24510ffd6756062914d7eb93490b
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-10-20 11:49:01 +0200

    leo19

--- src/test/java/leo19/term/chez/chezTest.kt
+++ src/test/java/leo19/term/chez/chezTest.kt
@@ -4,8 +4,11 @@ import leo.base.assertEqualTo
 import leo19.term.function
 import leo19.term.get
 import leo19.term.invoke
+import leo19.term.lhs
 import leo19.term.nullTerm
+import leo19.term.rhs
 import leo19.term.term
+import leo19.term.to
 import leo19.term.variable
 import kotlin.test.Test
 
@@ -29,6 +32,27 @@ class ChezTest {
 		term(term(10), term(20))
 			.chez
 			.assertEqualTo("(vector 10 20)")
+	}
+
+	@Test
+	fun pair() {
+		term(10).to(term(20))
+			.chez
+			.assertEqualTo("(cons 10 20)")
+	}
+
+	@Test
+	fun lhs() {
+		term(10).lhs
+			.chez
+			.assertEqualTo("(car 10)")
+	}
+
+	@Test
+	fun rhs() {
+		term(10).rhs
+			.chez
+			.assertEqualTo("(cdr 10)")
 	}
 
 	@Test
