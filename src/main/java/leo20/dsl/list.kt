14f9fd05b2fcd71b11eca601e7c5414c6fb69617
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-10-30 18:05:01 +0100

    leo20 - untyped

--- src/main/java/leo20/dsl/list.kt
+++ src/main/java/leo20/dsl/list.kt
@@ -49,4 +49,65 @@ val list_ = dsl_ {
 			}
 		}
 	}
+
+	define {
+		list { any }
+		fold { function { any } }
+		does {
+			recursively {
+				get { list }
+				switch {
+					empty {
+						does {
+							list { get { empty } }
+						}
+					}
+					link {
+						does {
+							get { link { previous { list } } }
+							fold { get { fold { function } } }
+							append {
+								get { fold { function } }
+								apply { get { link { last { item } } } }
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	test {
+		list { empty }
+		fold {
+			function {
+				item {
+					get { item { number } }
+					plus { number(1) }
+				}
+			}
+		}
+		equals_ {
+			list { empty }
+		}
+	}
+
+	test {
+		list { empty }
+		append { item { number(1) } }
+		append { item { number(2) } }
+		fold {
+			function {
+				item {
+					get { item { number } }
+					plus { number(10) }
+				}
+			}
+		}
+		equals_ {
+			list { empty }
+			append { item { number(11) } }
+			append { item { number(12) } }
+		}
+	}
 }
