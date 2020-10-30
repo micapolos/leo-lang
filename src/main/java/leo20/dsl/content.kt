8bbe03405cf49b88778c565ba56bdfc5c8d013d1
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-10-30 23:42:16 +0100

    leo20 - untyped

--- src/main/java/leo20/dsl/content.kt
+++ src/main/java/leo20/dsl/content.kt
@@ -12,7 +12,7 @@ val content_ = dsl_ {
 			y { any }
 			does { one }
 		}
-		content { x { do_ { y { z } } } }
-		equals_ { x { one } }
+		content { do_ { x { do_ { y { z } } } } }
+		equals_ { quote { do_ { x { one } } } }
 	}
 }
