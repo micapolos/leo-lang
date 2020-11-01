66211cf520ca36455a28b6550dd157e3f60146f7
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-11-02 00:01:25 +0100

    leo20 - untyped

--- src/main/java/leo21/typed/typed.kt
+++ src/main/java/leo21/typed/typed.kt
@@ -3,10 +3,18 @@ package leo21.typed
 import leo.base.fold
 import leo.base.notNullOrError
 import leo14.lambda.Term
+import leo14.lambda.invoke
+import leo14.lambda.term
+import leo14.lambda.value.DoubleMinusDoubleValue
+import leo14.lambda.value.DoublePlusDoubleValue
+import leo14.lambda.value.DoubleTimesDoubleValue
+import leo14.lambda.value.StringPlusStringValue
 import leo14.lambda.value.Value
 import leo21.type.ChoiceType
 import leo21.type.StructType
 import leo21.type.Type
+import leo21.type.doubleType
+import leo21.type.stringType
 import leo21.type.type
 
 data class Typed(val valueTerm: Term<Value>, val type: Type)
@@ -48,3 +56,31 @@ fun Typed.invoke(typed: Typed) = invokeOrNull(typed).notNullOrError("invoke")
 fun Typed.make(name: String) = typed(name lineTo this)
 
 fun Typed.plus(line: LineTyped): Typed = struct.plus(line).typed
+
+val Typed.stringValueTerm: Term<Value>
+	get() =
+		structOrNull!!.onlyLineOrNull!!.stringTypedOrNull!!.valueTerm
+
+val Typed.doubleValueTerm: Term<Value>
+	get() =
+		structOrNull!!.onlyLineOrNull!!.doubleTypedOrNull!!.valueTerm
+
+fun Typed.doublePlus(typed: Typed): Typed =
+	Typed(
+		term(DoublePlusDoubleValue).invoke(doubleValueTerm).invoke(typed.doubleValueTerm),
+		doubleType)
+
+fun Typed.doubleMinus(typed: Typed): Typed =
+	Typed(
+		term(DoubleMinusDoubleValue).invoke(doubleValueTerm).invoke(typed.doubleValueTerm),
+		doubleType)
+
+fun Typed.doubleTimes(typed: Typed): Typed =
+	Typed(
+		term(DoubleTimesDoubleValue).invoke(doubleValueTerm).invoke(typed.doubleValueTerm),
+		doubleType)
+
+fun Typed.stringPlus(typed: Typed): Typed =
+	Typed(
+		term(StringPlusStringValue).invoke(stringValueTerm).invoke(typed.stringValueTerm),
+		stringType)
