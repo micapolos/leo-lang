7e792601aed4ec9d9657c908f86888f7ee0c5551
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-11-02 11:26:45 +0100

    leo20 - untyped

---
+++ src/main/java/leo21/type/scriptType.kt
@@ -0,0 +1,71 @@
+package leo21.type
+
+import leo.base.fold
+import leo.base.notNullIf
+import leo.base.notNullOrError
+import leo.base.reverse
+import leo14.FieldScriptLine
+import leo14.LinkScript
+import leo14.LiteralScriptLine
+import leo14.Script
+import leo14.ScriptField
+import leo14.ScriptLine
+import leo14.UnitScript
+import leo14.fieldOrNull
+import leo14.isEmpty
+import leo14.lineSeq
+import leo14.onlyLineOrNull
+
+val Script.type: Type
+	get() =
+		choiceTypeOrNull ?: type(struct)
+
+val Script.choiceTypeOrNull: Type?
+	get() =
+		onlyLineOrNull?.fieldOrNull?.let { field ->
+			notNullIf(field.string == "choice") {
+				type(field.rhs.choice)
+			}
+		}
+
+val Script.struct: Struct
+	get() =
+		struct().fold(lineSeq.reverse) { plus(it.typeLine) }
+
+val Script.choice: Choice
+	get() =
+		choice().fold(lineSeq.reverse) { plus(it.typeLine) }
+
+val ScriptLine.typeLine: Line
+	get() =
+		when (this) {
+			is LiteralScriptLine -> error("literal not a type")
+			is FieldScriptLine -> field.typeLine
+		}
+
+val ScriptField.typeLine: Line
+	get() =
+		keywordTypeLineOrNull ?: rawTypeLine
+
+val ScriptField.keywordTypeLineOrNull: Line?
+	get() =
+		when (string) {
+			"number" -> notNullIf(rhs.isEmpty) { doubleLine }
+			"text" -> notNullIf(rhs.isEmpty) { stringLine }
+			"function" -> line(rhs.arrowLine)
+			else -> null
+		}
+
+val ScriptField.rawTypeLine: Line
+	get() =
+		string lineTo rhs.type
+
+val Script.arrowLine: Arrow
+	get() =
+		when (this) {
+			is UnitScript -> error("not a function")
+			is LinkScript -> link.line.fieldOrNull.notNullOrError("not a function").let { field ->
+				if (field.string != "doing") error("not a function")
+				else link.lhs.type arrowTo field.rhs.type
+			}
+		}
---
+++ src/test/java/leo21/type/scriptTypeTest.kt
@@ -0,0 +1,81 @@
+package leo21.type
+
+import leo.base.assertEqualTo
+import leo14.lineTo
+import leo14.literal
+import leo14.script
+import kotlin.test.Test
+import kotlin.test.assertFails
+
+class ScriptTypeTest {
+	@Test
+	fun empty() {
+		script().type.assertEqualTo(type())
+	}
+
+	@Test
+	fun number() {
+		script("number").type.assertEqualTo(doubleType)
+	}
+
+	@Test
+	fun text() {
+		script("text").type.assertEqualTo(stringType)
+	}
+
+	@Test
+	fun literals() {
+		assertFails { script(literal(20.0)).type }
+		assertFails { script(literal("foo")).type }
+	}
+
+	@Test
+	fun struct() {
+		script(
+			"name" lineTo script("text"),
+			"age" lineTo script("number"))
+			.type
+			.assertEqualTo(
+				type(
+					"name" lineTo stringType,
+					"age" lineTo doubleType))
+	}
+
+	@Test
+	fun choice() {
+		script(
+			"choice" lineTo script(
+				"number" lineTo script(),
+				"text" lineTo script()))
+			.type
+			.assertEqualTo(type(choice(doubleLine, stringLine)))
+	}
+
+	@Test
+	fun function() {
+		script(
+			"function" lineTo script(
+				"number" lineTo script(),
+				"doing" lineTo script("text")))
+			.type
+			.assertEqualTo(type(line(doubleType arrowTo stringType)))
+	}
+
+	@Test
+	fun function_empty() {
+		assertFails {
+			script("function" lineTo script()).type
+		}
+	}
+
+	@Test
+	fun function_noDoing() {
+		assertFails {
+			script(
+				"function" lineTo script(
+					"number" lineTo script(),
+					"making" lineTo script("text")))
+				.type
+		}
+	}
+}
