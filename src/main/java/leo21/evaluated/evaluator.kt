8c4a4bfc76eb43b2be512660b5b64c19cae46cd1
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-11-03 22:21:51 +0100

    leo21

--- src/main/java/leo21/evaluated/evaluator.kt
+++ src/main/java/leo21/evaluated/evaluator.kt
@@ -3,6 +3,7 @@ package leo21.evaluated
 import leo.base.fold
 import leo.base.reverse
 import leo14.Script
+import leo14.ScriptField
 import leo14.ScriptLine
 import leo14.lineSeq
 import leo21.compiled.Compiled
@@ -18,6 +19,23 @@ fun Evaluator.plus(script: Script): Evaluator =
 
 fun Evaluator.plus(scriptLine: ScriptLine): Evaluator =
 	Compiled(context.compiledScope, evaluated.typed).plus(scriptLine).body
+		.let { body ->
+			body.term.value.let { value ->
+				Evaluator(context, Evaluated(value, body.type))
+			}
+		}
+
+fun Evaluator.plus(scriptField: ScriptField): Evaluator =
+	when (scriptField.string) {
+		"define" -> plusDefine(scriptField.rhs)
+		else -> plusEvaluate(scriptField)
+	}
+
+fun Evaluator.plusDefine(rhs: Script): Evaluator =
+	TODO()
+
+fun Evaluator.plusEvaluate(scriptField: ScriptField): Evaluator =
+	Compiled(context.compiledScope, evaluated.typed).plus(scriptField).body
 		.let { body ->
 			body.term.value.let { value ->
 				Evaluator(context, Evaluated(value, body.type))
