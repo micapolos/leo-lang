b6efb9e6df214858f1c8d100532e3de898724c3e
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-11-04 10:36:18 +0100

    leo21

--- src/main/java/leo21/dictionary/dictionary.kt
+++ src/main/java/leo21/dictionary/dictionary.kt
@@ -1,6 +1,10 @@
 package leo21.dictionary
 
 import leo13.Stack
-import leo20.Definition
+import leo13.stack
 
 data class Dictionary(val definitionStack: Stack<Definition>)
+
+val emptyDictionary = Dictionary(stack())
+fun Dictionary.plus(definition: Definition) = Dictionary(definitionStack)
+
