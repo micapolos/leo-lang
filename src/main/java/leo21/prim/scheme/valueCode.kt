9f462a86c09fce2dd4f14d96777fddc76202b9af
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-11-04 22:46:16 +0100

    leo21

--- src/main/java/leo21/prim/scheme/valueCode.kt
+++ src/main/java/leo21/prim/scheme/valueCode.kt
@@ -2,5 +2,8 @@ package leo21.prim.scheme
 
 import leo14.lambda.scheme.code
 
+val firstCode = code("(lambda (a) (lambda (b) a))")
+val secondCode = code("(lambda (a) (lambda (b) b))")
+
 fun fn2Code(op: String) =
-	code("(lambda (x) ($op (x (lambda (a) (lambda (b) a))) (x (lambda (a) (lambda (b) b))))")
+	code("(lambda (x) ($op (x $firstCode) (x $secondCode)))")
