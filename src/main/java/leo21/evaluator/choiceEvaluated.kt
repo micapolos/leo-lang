0e4a9a5f88e498a7c92093ac9affe791f2b3b42e
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-11-15 23:57:19 +0100

    leo21

--- src/main/java/leo21/evaluator/choiceEvaluated.kt
+++ src/main/java/leo21/evaluator/choiceEvaluated.kt
@@ -4,18 +4,21 @@ import leo14.lambda.value.Value
 import leo14.lambda.value.switch
 import leo21.prim.Prim
 import leo21.type.Choice
+import leo21.type.Type
+import leo21.type.choice
 import leo21.type.linkOrNull
 
-data class ChoiceEvaluated(val value: Value<Prim>, val choice: Choice)
+data class ChoiceEvaluated(val value: Value<Prim>?, val choice: Choice)
 
 infix fun Value<Prim>.of(choice: Choice) = ChoiceEvaluated(this, choice)
+val emptyChoiceEvaluated = ChoiceEvaluated(null, choice())
 
 fun <R> ChoiceEvaluated.switch(
 	firstFn: (ChoiceEvaluated) -> R,
 	secondFn: (LineEvaluated) -> R
 ): R =
 	choice.linkOrNull!!.let { choiceLink ->
-		value.switch(
+		value!!.switch(
 			{ tailValue ->
 				firstFn(tailValue of choiceLink.tail)
 			},
@@ -23,3 +26,9 @@ fun <R> ChoiceEvaluated.switch(
 				secondFn(headValue of choiceLink.head)
 			})
 	}
+
+fun ChoiceEvaluated.plusNotChosen(type: Type): ChoiceEvaluated =
+	TODO()
+
+fun ChoiceEvaluated.plusChosen(lineEvaluated: LineEvaluated): ChoiceEvaluated =
+	TODO()
