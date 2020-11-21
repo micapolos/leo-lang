b3af652c42ee469d92c3055eebad16917f58d9b7
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-11-21 11:30:02 +0100

    leo21

--- src/main/java/leo14/scriptError.kt
+++ src/main/java/leo14/scriptError.kt
@@ -7,5 +7,7 @@ data class ScriptError(val script: Script) : Error()
 val Script.error: Nothing get() = throw ScriptError(this)
 fun error(vararg lines: ScriptLine): Nothing = script(*lines).error
 fun error(f: F): Nothing = script_(f).error
-fun <R> R?.orError(f: F) = this ?: error(f)
-fun <R> R?.orError(vararg lines: ScriptLine) = this ?: error(*lines)
+
+fun <R> R?.orError(script: Script) = this ?: script.error
+fun <R> R?.orError(vararg lines: ScriptLine) = orError(script(*lines))
+fun <R> R?.orError(f: F) = orError(script_(f))
