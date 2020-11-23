a298f7c3997a9409d103bbb7ef7590dd697c85f7
Michał Pociecha-Łoś <micapolos@gmail.com>
2020-11-23 23:33:58 +0100

    leo21

--- src/test/java/leo21/reflect/reflectTest.kt
+++ src/test/java/leo21/reflect/reflectTest.kt
@@ -1,12 +1,12 @@
 package leo21.reflect
 
-import leo.base.assertEqualTo
+import leo.base.assertNotNull
 import leo21.token.processor.emptyBodyProcessor
 import kotlin.test.Test
 
 class ReflectTest {
 	@Test
 	fun stack() {
-		emptyBodyProcessor.refScriptLine.assertEqualTo(null)
+		emptyBodyProcessor.refScriptLine.assertNotNull
 	}
 }
