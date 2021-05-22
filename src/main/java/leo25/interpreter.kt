263e14a2361f100e1308de5dc683b9f2bdcdceb4
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-05-22 16:39:39 +0200

    leo25

--- src/main/java/leo25/interpreter.kt
+++ src/main/java/leo25/interpreter.kt
@@ -1,6 +1,7 @@
 package leo25
 
 import leo.base.fold
+import leo.base.notNullIf
 import leo.base.reverse
 import leo14.*
 import leo25.parser.scriptOrNull
@@ -14,13 +15,19 @@ fun Context.interpreter(value: Value = value()) =
 	Interpreter(this, value)
 
 fun Context.interpretedValue(script: Script): Value =
-	interpreter().fold(script.lineSeq.reverse) { plus(it) }.value
+	interpreter(script).value
 
 val Script.interpret: Script
 	get() =
 		context().interpretedValue(this).script
 
 val String.interpret: String get() = scriptOrNull?.interpret?.string ?: this
+
+fun Context.interpreter(script: Script): Interpreter =
+	interpreter().plus(script)
+
+fun Interpreter.plus(script: Script): Interpreter =
+	fold(script.lineSeq.reverse) { plus(it) }
 
 fun Interpreter.plus(scriptLine: ScriptLine): Interpreter =
 	when (scriptLine) {
@@ -37,22 +44,28 @@ fun Interpreter.plusStaticOrNull(scriptField: ScriptField): Interpreter? =
 	when (scriptField.string) {
 		doName -> plusDo(scriptField.rhs)
 		doingName -> plusGiving(scriptField.rhs)
+		evaluateName -> plusEvaluateOrNull(scriptField.rhs)
 		letName -> plusLet(scriptField.rhs)
 		switchName -> plusSwitchOrNull(scriptField.rhs)
 		else -> null
 	}
 
-fun Interpreter.plusDo(script: Script): Interpreter =
-	context.interpreter(context.apply(block(script), value))
+fun Interpreter.plusDo(rhs: Script): Interpreter =
+	context.interpreter(context.apply(block(rhs), value))
 
-fun Interpreter.plusGiving(script: Script): Interpreter =
-	plus(line(context.function(body(script))))
+fun Interpreter.plusEvaluateOrNull(rhs: Script): Interpreter? =
+	notNullIf(rhs.isEmpty) {
+		context.interpreter(context.interpretedValue(rhs))
+	}
 
-fun Interpreter.plusLet(script: Script): Interpreter =
-	context.define(script).interpreter(value)
+fun Interpreter.plusGiving(rhs: Script): Interpreter =
+	plus(line(context.function(body(rhs))))
 
-fun Interpreter.plusSwitchOrNull(script: Script): Interpreter? =
-	context.switchOrNull(value, script)?.let { context.interpreter(it) }
+fun Interpreter.plusLet(rhs: Script): Interpreter =
+	context.define(rhs).interpreter(value)
+
+fun Interpreter.plusSwitchOrNull(rhs: Script): Interpreter? =
+	context.switchOrNull(value, rhs)?.let { context.interpreter(it) }
 
 fun Interpreter.plusDynamic(scriptField: ScriptField): Interpreter =
 	plus(scriptField.string lineTo context.interpretedValue(scriptField.rhs))
