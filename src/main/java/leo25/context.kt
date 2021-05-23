3d7b234116beb989ccd4cb908b45911acb2b55e2
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-05-23 15:07:04 +0200

    leo25

--- src/main/java/leo25/context.kt
+++ src/main/java/leo25/context.kt
@@ -9,3 +9,6 @@ fun context(dictionary: Dictionary, module: Module) = Context(dictionary, module
 
 fun context() = context(dictionary(), module())
 val nativeContext get() = context(nativeDictionary, module())
+
+fun Context.plus(definition: Definition): Context =
+	context(dictionary.plus(definition), module.plus(definition))
--- src/main/java/leo25/module.kt
+++ src/main/java/leo25/module.kt
@@ -1,9 +1,11 @@
 package leo25
 
 import leo13.Stack
+import leo13.push
 import leo13.stack
 
 data class Module(val definitionStack: Stack<Definition>)
 
 fun module(definitionStack: Stack<Definition>) = Module(definitionStack)
 fun module(vararg definitions: Definition) = module(stack(*definitions))
+operator fun Module.plus(definition: Definition) = module(definitionStack.push(definition))
