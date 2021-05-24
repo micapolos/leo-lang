92e01789c52037c6b02a695ba6548d3ba9d5514e
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-05-24 23:24:10 +0200

    leo 25

--- src/main/java/leo25/nativeNames.kt
+++ src/main/java/leo25/nativeNames.kt
@@ -1,10 +1,5 @@
 package leo25
 
 val plusName = "plus"
-val hashName = "hash"
-val noName = "no"
-val numberName = "number"
 val minusName = "minus"
 val timesName = "times"
-val textName = "text"
-val yesName = "yes"
