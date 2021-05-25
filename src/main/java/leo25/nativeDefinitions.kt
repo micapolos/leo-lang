8593deff352992cf69d7189feffc73600d8545fd
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-05-25 07:56:28 +0200

    leo 25

--- src/main/java/leo25/nativeDefinitions.kt
+++ src/main/java/leo25/nativeDefinitions.kt
@@ -24,39 +24,58 @@ val Number.nativeValue get() = value(field(literal(this)))
 val textAppendTextDefinition
 	get() =
 		nativeDefinition(
-			script(textAnyLine, appendName lineTo script(textAnyLine))
-		) {
-			nativeValue(textName).nativeText
-				.plus(nativeValue(appendName).nativeValue(textName).nativeText)
-				.nativeValue
-		}
+			textName, { nativeText },
+			appendName,
+			textName, { nativeText },
+			{ nativeValue }, String::plus
+		)
 
 val numberPlusNumberDefinition
 	get() =
 		nativeDefinition(
-			script(numberAnyLine, plusName lineTo script(numberAnyLine))
-		) {
-			nativeValue(numberName).nativeNumber
-				.plus(nativeValue(plusName).nativeValue(numberName).nativeNumber)
-				.nativeValue
-		}
+			numberName, { nativeNumber },
+			plusName,
+			numberName, { nativeNumber },
+			{ nativeValue }, Number::plus
+		)
 
 val numberMinusNumberDefinition
 	get() =
 		nativeDefinition(
-			script(numberAnyLine, minusName lineTo script(numberAnyLine))
-		) {
-			nativeValue(numberName).nativeNumber
-				.minus(nativeValue(minusName).nativeValue(numberName).nativeNumber)
-				.nativeValue
-		}
+			numberName, { nativeNumber },
+			minusName,
+			numberName, { nativeNumber },
+			{ nativeValue }, Number::minus
+		)
 
 val numberTimesNumberDefinition
 	get() =
 		nativeDefinition(
-			script(numberAnyLine, timesName lineTo script(numberAnyLine))
-		) {
-			nativeValue(numberName).nativeNumber
-				.times(nativeValue(timesName).nativeValue(numberName).nativeNumber)
-				.nativeValue
-		}
+			numberName, { nativeNumber },
+			timesName,
+			numberName, { nativeNumber },
+			{ nativeValue }, Number::times
+		)
+
+
+inline fun <L, R, O> nativeDefinition(
+	lhsType: String,
+	crossinline lhsFn: Value.() -> L,
+	opName: String,
+	rhsType: String,
+	crossinline rhsFn: Value.() -> R,
+	crossinline retFn: O.() -> Value,
+	crossinline fn: L.(R) -> O
+): Definition =
+	nativeDefinition(
+		script(
+			lhsType lineTo script(anyName),
+			opName lineTo script(
+				rhsType lineTo script(anyName)
+			)
+		)
+	) {
+		nativeValue(lhsType).lhsFn()
+			.fn(nativeValue(opName).nativeValue(rhsType).rhsFn())
+			.retFn()
+	}
