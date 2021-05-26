71073feeeb75475d3fa181d1db38064ccd75f494
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-05-27 01:33:10 +0200

    leo 25

--- src/main/java/leo25/main.kt
+++ src/main/java/leo25/main.kt
@@ -3,17 +3,15 @@ package leo25
 import leo.base.print
 import leo.java.io.file
 import leo.java.io.inString
-import leo13.linkOrNull
-import leo13.stack
 import java.io.File
 
 fun main(args: Array<String>) {
 	val string =
 		if (args.isEmpty()) inString
 		else try {
-			Use(stack(*args).linkOrNull!!).path.file.readText()
-		} catch (e: Exception) {
 			File(args[0]).readText()
+		} catch (e: Exception) {
+			use(args[0], *args.copyOfRange(1, args.size)).path.file.readText()
 		}
 	string.interpret.print
 }
