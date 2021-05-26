b1eca35ca2cd538b19487dda5e7ad4775e9df717
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-05-27 01:35:28 +0200

    leo 25

--- src/main/java/leo25/main.kt
+++ src/main/java/leo25/main.kt
@@ -6,12 +6,13 @@ import leo.java.io.inString
 import java.io.File
 
 fun main(args: Array<String>) {
-	val string =
-		if (args.isEmpty()) inString
-		else try {
-			File(args[0]).readText()
-		} catch (e: Exception) {
-			use(args[0], *args.copyOfRange(1, args.size)).path.file.readText()
-		}
-	string.interpret.print
+	readText(args).interpret.print
 }
+
+fun readText(args: Array<String>) =
+	if (args.isEmpty()) inString
+	else try {
+		File(args[0]).readText()
+	} catch (e: Exception) {
+		use(args[0], *args.copyOfRange(1, args.size)).path.file.readText()
+	}
